9d7c3190cc346bf2a5576b6b93c26723059ae9a1
Roger Braun <roger@rogerbraun.net>
2017-04-30 11:16:41 +0200

    Get create activity from created object id.
    This is useful for Ostatus federation because ostatus doesn't have
    different ids for objects and activities...

--- lib/pleroma/activity.ex
+++ lib/pleroma/activity.ex
@@ -18,4 +18,9 @@ defmodule Pleroma.Activity do
     Repo.all(from activity in Activity,
       where: fragment("? @> ?", activity.data, ^%{object: %{id: ap_id}}))
   end
+
+  def get_create_activity_by_object_ap_id(ap_id) do
+    Repo.one(from activity in Activity,
+      where: fragment("? @> ?", activity.data, ^%{type: "Create", object: %{id: ap_id}}))
+  end
 end
