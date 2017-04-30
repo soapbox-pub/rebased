11ea08649d5a5e5d2ac9ee29406f53240be77ec4
Roger Braun <roger@rogerbraun.net>
2017-04-30 10:06:57 +0200

    Make cache bigger and longer lived.

--- lib/pleroma/application.ex
+++ lib/pleroma/application.ex
@@ -15,9 +15,9 @@ defmodule Pleroma.Application do
       # Start your own worker by calling: Pleroma.Worker.start_link(arg1, arg2, arg3)
       # worker(Pleroma.Worker, [arg1, arg2, arg3]),
       worker(Cachex, [:user_cache, [
-                         default_ttl: 5000,
+                         default_ttl: 25000,
                          ttl_interval: 1000,
-                         limit: 500
+                         limit: 2500
                        ]])
     ]
 
