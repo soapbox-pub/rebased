83c657afa557ac3cdfd2e3888511b403857de8aa
Roger Braun <roger@rogerbraun.net>
2017-06-14 14:46:18 +0200

    Do some basic escaping.

---
+++ test/web/twitter_api/twitter_api_utils_test.exs
@@ -0,0 +1,14 @@
+defmodule Pleroma.Web.TwitterAPI.UtilsTest do
+  alias Pleroma.Web.TwitterAPI.Utils
+  use Pleroma.DataCase
+
+  test "it adds attachment links to a given text and attachment set" do
+    attachment = %{
+      "url" => [%{"href" => "http://heise.de/i\"m a boy.png"}]
+    }
+
+    res = Utils.add_attachments("", [attachment])
+
+    assert res == "<br>\n<a href=\"http://heise.de/i%22m%20a%20boy.png\" class='attachment'>i\"m a boy.png</a>"
+  end
+end
