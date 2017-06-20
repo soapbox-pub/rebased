08e53df91e1b52a36d557668d16afecc2eaa0a74
dtluna <dtluna@openmailbox.org>
2017-06-20 17:55:57 +0300

    Move util functions to UtilController

---
+++ lib/pleroma/web/twitter_api/controllers/util_controller.ex
@@ -0,0 +1,18 @@
+defmodule Pleroma.Web.TwitterAPI.UtilController do
+  use Pleroma.Web, :controller
+  alias Pleroma.Web
+
+  def help_test(conn, _params) do
+    json(conn, "ok")
+  end
+
+  def config(conn, _params) do
+    json(conn, %{
+          site: %{
+            name: Web.base_url,
+            server: Web.base_url,
+            textlimit: -1
+          }
+    })
+  end
+end
