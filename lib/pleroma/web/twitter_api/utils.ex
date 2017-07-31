2eba8e74ccf2de0ad65b213bb166917c32db9d68
Roger Braun <roger@rogerbraun.net>
2017-07-31 10:46:29 +0200

    Close br tags.

--- lib/pleroma/web/twitter_api/utils.ex
+++ lib/pleroma/web/twitter_api/utils.ex
@@ -15,13 +15,13 @@ defmodule Pleroma.Web.TwitterAPI.Utils do
         "<a href=\"#{URI.encode(href)}\" class='attachment'>#{Path.basename(href)}</a>"
       _ -> ""
     end)
-    Enum.join([text | attachment_text], "<br>\n")
+    Enum.join([text | attachment_text], "<br />\n")
   end
 
   def format_input(text, mentions) do
     HtmlSanitizeEx.strip_tags(text)
     |> Formatter.linkify
-    |> String.replace("\n", "<br>\n")
+    |> String.replace("\n", "<br />\n")
     |> add_user_links(mentions)
   end
 
