f0257c751611f4eed076c69260a5dc4ef81a6728
Roger Braun <roger@rogerbraun.net>
2017-09-16 11:06:55 +0200

    Add fts search index.

---
+++ priv/repo/migrations/20170916090107_add_fts_index_to_activities.exs
@@ -0,0 +1,8 @@
+defmodule Pleroma.Repo.Migrations.AddFTSIndexToActivities do
+  use Ecto.Migration
+  @disable_ddl_transaction true
+
+  def change do
+    create index(:activities, ["(to_tsvector('english', data->'object'->>'content'))"], concurrently: true, using: :gin, name: :activities_fts)
+  end
+end
