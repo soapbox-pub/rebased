62ca2617cda39f9dcafcdd1cc64d1b2ea38271cd
Roger Braun <roger@rogerbraun.net>
2017-09-17 14:05:53 +0200

    Add tag index.
    This still isn't optimal, but at least it isn't super slow.

---
+++ priv/repo/migrations/20170917120416_add_tag_index.exs
@@ -0,0 +1,9 @@
+defmodule Pleroma.Repo.Migrations.AddTagIndex do
+  use Ecto.Migration
+
+  @disable_ddl_transaction true
+
+  def change do
+    create index(:activities, ["(data #> '{\"object\",\"tag\"}')"], concurrently: true, using: :gin, name: :activities_tags)
+  end
+end
