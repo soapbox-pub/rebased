261ec824515c1de6d93ad1c4ca7d3cf73faa58a4
Roger Braun <roger@rogerbraun.net>
2017-10-24 08:39:24 +0200

    Return nil for nil object requests.

--- lib/pleroma/object.ex
+++ lib/pleroma/object.ex
@@ -21,6 +21,7 @@ defmodule Pleroma.Object do
     |> unique_constraint(:ap_id, name: :objects_unique_apid_index)
   end
 
+  def get_by_ap_id(nil), do: nil
   def get_by_ap_id(ap_id) do
     Repo.one(from object in Object,
       where: fragment("? @> ?", object.data, ^%{id: ap_id}))
