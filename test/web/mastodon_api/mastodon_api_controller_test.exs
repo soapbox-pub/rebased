c56d28f96c343d445ec8d06baf351423691036f3
eal <eal@waifu.club>
2017-10-29 00:07:38 +0300

    Fix return type of /api/v1/follows

--- test/web/mastodon_api/mastodon_api_controller_test.exs
+++ test/web/mastodon_api/mastodon_api_controller_test.exs
@@ -287,7 +287,7 @@ defmodule Pleroma.Web.MastodonAPI.MastodonAPIControllerTest do
     |> assign(:user, user)
     |> post("/api/v1/follows", %{"uri" => other_user.nickname})
 
-    assert %{"id" => id, "following" => true} = json_response(conn, 200)
+    assert other_user = json_response(conn, 200)
   end
 
   test "unimplemented block/mute endpoints" do
