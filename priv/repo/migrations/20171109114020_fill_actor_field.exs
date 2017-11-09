9e60de695db74bbe66c2be411ec85420b0ff54a7
Roger Braun <rbraun@Bobble.local>
2017-11-09 13:45:17 +0100

    Fix migration for empty db.

--- priv/repo/migrations/20171109114020_fill_actor_field.exs
+++ priv/repo/migrations/20171109114020_fill_actor_field.exs
@@ -5,18 +5,19 @@ defmodule Pleroma.Repo.Migrations.FillActorField do
 
   def up do
     max = Repo.aggregate(Activity, :max, :id)
-    IO.puts("#{max} activities")
-    chunks = 0..(round(max / 10_000))
+    if max do
+      IO.puts("#{max} activities")
+      chunks = 0..(round(max / 10_000))
 
-    Enum.each(chunks, fn (i) ->
-      min = i * 10_000
-      max = min + 10_000
-      IO.puts("Updating #{min}")
-      execute("""
+      Enum.each(chunks, fn (i) ->
+        min = i * 10_000
+        max = min + 10_000
+        execute("""
         update activities set actor = data->>'actor' where id > #{min} and id <= #{max};
-      """)
-      |> IO.inspect
-    end)
+        """)
+        |> IO.inspect
+      end)
+    end
   end
 
   def down do
