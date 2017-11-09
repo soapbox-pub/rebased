20b8b8774345d07537687ad5dd2fdb6f809e684f
Roger Braun <rbraun@Bobble.local>
2017-11-09 12:33:38 +0100

    Update in chunks.

--- priv/repo/migrations/20171109091239_add_actor_to_activity.exs
+++ priv/repo/migrations/20171109091239_add_actor_to_activity.exs
@@ -1,6 +1,8 @@
 defmodule Pleroma.Repo.Migrations.AddActorToActivity do
   use Ecto.Migration
 
+  alias Pleroma.{Repo, Activity}
+
   @disable_ddl_transaction true
 
   def up do
@@ -8,9 +10,18 @@ defmodule Pleroma.Repo.Migrations.AddActorToActivity do
       add :actor, :string
     end
 
-    execute """
-      update activities set actor = data->>'actor';
-    """
+    max = Repo.aggregate(Activity, :max, :id)
+    IO.puts("#{max} activities")
+    chunks = 0..(round(max / 10_000))
+
+    Enum.each(chunks, fn (i) ->
+      min = i * 10_000
+      max = min + 10_000
+      IO.puts("Updating #{min}")
+      execute """
+        update activities set actor = data->>'actor' where id > #{min} and id <= #{max};
+      """
+    end)
 
     create index(:activities, [:actor, "id DESC NULLS LAST"], concurrently: true)
   end
