73249fa5ff25c3f40ca79dfb9e4ce49b0dc1326b
Calv Collins <calvcoll@gmail.com>
2018-02-08 18:15:59 +0000

    Changed fallback controller to handle all cases from OAuthController

--- lib/pleroma/web/oauth/fallback_controller.ex
+++ lib/pleroma/web/oauth/fallback_controller.ex
@@ -2,15 +2,8 @@ defmodule Pleroma.Web.OAuth.FallbackController do
     use Pleroma.Web, :controller
     alias Pleroma.Web.OAuth.OAuthController
 
-    # No user
-    def call(conn, nil) do
-        conn
-        |> put_flash(:error, "Invalid Username/Password")
-        |> OAuthController.authorize(conn.params)
-    end
-
-    # No password
-    def call(conn, false) do
+    # No user/password
+    def call(conn, _) do
         conn
         |> put_flash(:error, "Invalid Username/Password")
         |> OAuthController.authorize(conn.params)
