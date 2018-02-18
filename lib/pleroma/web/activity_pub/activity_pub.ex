932d346d35e0b16537ee2527ca2253f00c214c12
lain <lain@soykaf.club>
2018-02-18 23:13:19 +0100

    fix typo.

--- lib/pleroma/web/activity_pub/activity_pub.ex
+++ lib/pleroma/web/activity_pub/activity_pub.ex
@@ -272,9 +272,8 @@ defmodule Pleroma.Web.ActivityPub.ActivityPub do
         name: data["name"]
       }
 
-      User.insert_or_update_user(user_data)
       if user = User.get_by_ap_id(ap_id) do
-        User.info_changeset(user, user_data}
+        User.info_changeset(user, user_data)
         |> Repo.update
       else
         User.insert_or_update_user(user_data)
