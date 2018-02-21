947ba6495d556f621d0e0b517993f8200be9ccd9
lain <lain@soykaf.club>
2018-02-21 10:31:13 +0100

    More TwAPI fixes.

--- lib/pleroma/web/twitter_api/twitter_api.ex
+++ lib/pleroma/web/twitter_api/twitter_api.ex
@@ -289,7 +289,7 @@ defmodule Pleroma.Web.TwitterAPI.TwitterAPI do
     actor = get_in(activity.data, ["actor"])
     user = User.get_cached_by_ap_id(actor)
     # mentioned_users = Repo.all(from user in User, where: user.ap_id in ^activity.data["to"])
-    mentioned_users = Enum.map(activity.data["to"] || [], fn (ap_id) ->
+    mentioned_users = Enum.map(activity.recipients || [], fn (ap_id) ->
       if ap_id do
         User.get_cached_by_ap_id(ap_id)
       else
