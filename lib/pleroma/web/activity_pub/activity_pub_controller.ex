9a4d400ff4d2c5c860bcb6582db01879cafd7467
lain <lain@soykaf.club>
2018-02-24 12:49:56 +0100

    Fix remote following.

--- lib/pleroma/web/activity_pub/activity_pub_controller.ex
+++ lib/pleroma/web/activity_pub/activity_pub_controller.ex
@@ -12,14 +12,18 @@ defmodule Pleroma.Web.ActivityPub.ActivityPubController do
   def user(conn, %{"nickname" => nickname}) do
     with %User{} = user <- User.get_cached_by_nickname(nickname),
          {:ok, user} <- Pleroma.Web.WebFinger.ensure_keys_present(user) do
-      json(conn, UserView.render("user.json", %{user: user}))
+      conn
+      |> put_resp_header("content-type", "application/activity+json")
+      |> json(UserView.render("user.json", %{user: user}))
     end
   end
 
   def object(conn, %{"uuid" => uuid}) do
     with ap_id <- o_status_url(conn, :object, uuid),
          %Object{} = object <- Object.get_cached_by_ap_id(ap_id) do
-      json(conn, ObjectView.render("object.json", %{object: object}))
+      conn
+      |> put_resp_header("content-type", "application/activity+json")
+      |> json(ObjectView.render("object.json", %{object: object}))
     end
   end
 
