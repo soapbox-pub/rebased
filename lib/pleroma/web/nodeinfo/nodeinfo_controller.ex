67dadd954ee0bc0c42b95a9ca0e471051a75a7b6
Dashie <dashie@sigpipe.me>
2018-05-02 21:31:42 +0200

    Add first try at nodeinfo

---
+++ lib/pleroma/web/nodeinfo/nodeinfo_controller.ex
@@ -0,0 +1,53 @@
+defmodule Pleroma.Web.Nodeinfo.NodeinfoController do
+  use Pleroma.Web, :controller
+
+  alias Pleroma.Web.Nodeinfo
+  alias Pleroma.Stats
+  alias Pleroma.Web
+
+  @instance Application.get_env(:pleroma, :instance)
+
+  def schemas(conn, _params) do
+    response = %{
+      links: [
+        %{
+          rel: "http://nodeinfo.diaspora.software/ns/schema/2.0",
+          href: Web.base_url() <> "/nodeinfo/2.0"
+        }
+      ]
+    }
+
+    json(conn, response)
+  end
+
+  # Schema definition: https://github.com/jhass/nodeinfo/blob/master/schemas/2.0/schema.json
+  def nodeinfo(conn, %{"version" => "2.0"}) do
+    response = %{
+      version: "2.0",
+      software: %{
+        name: "pleroma",
+        version: "#{Keyword.get(@instance, :version)})"
+      },
+      protocols: ["ostatus", "activitypub"],
+      services: %{
+        inbound: [],
+        outbound: []
+      },
+      openRegistrations: Keyword.get(@instance, :registrations_open),
+      usage: %{
+        users: %{
+          total: Stats.get_stats().user_count,
+        }
+      },
+      metadata: %{}
+    }
+
+    json(conn, response)
+  end
+
+  def nodeinfo(conn, _) do
+    conn
+    |> put_status(404)
+    |> json(%{error: "Nodeinfo schema not handled"})
+  end
+end
