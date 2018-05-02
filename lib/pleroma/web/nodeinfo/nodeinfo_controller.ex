fd0bf3aa71a67afab3cfe55d17d7dce9e2f2b305
Dashie <dashie@sigpipe.me>
2018-05-02 22:44:13 +0200

    Simplify version and fix typo

--- lib/pleroma/web/nodeinfo/nodeinfo_controller.ex
+++ lib/pleroma/web/nodeinfo/nodeinfo_controller.ex
@@ -26,7 +26,7 @@ defmodule Pleroma.Web.Nodeinfo.NodeinfoController do
       version: "2.0",
       software: %{
         name: "pleroma",
-        version: "#{Keyword.get(@instance, :version)})"
+        version: Keyword.get(@instance, :version)
       },
       protocols: ["ostatus", "activitypub"],
       services: %{
