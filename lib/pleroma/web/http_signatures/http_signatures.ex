bf64208b524482e44407840b7aa66dd896c05def
lain <lain@soykaf.club>
2018-05-21 12:09:03 +0200

    Add missing alias.

--- lib/pleroma/web/http_signatures/http_signatures.ex
+++ lib/pleroma/web/http_signatures/http_signatures.ex
@@ -2,6 +2,7 @@
 defmodule Pleroma.Web.HTTPSignatures do
   alias Pleroma.User
   alias Pleroma.Web.ActivityPub.Utils
+  alias Pleroma.Web.ActivityPub
   require Logger
 
   def split_signature(sig) do
