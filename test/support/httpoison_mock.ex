7fbcd4caaf877749c13d6755eaaa956ac0ea5012
William Pitcock <nenolod@dereferenced.org>
2018-08-14 17:07:01 +0000

    test: add kroeg fixtures

--- test/support/httpoison_mock.ex
+++ test/support/httpoison_mock.ex
@@ -4,6 +4,18 @@ defmodule HTTPoisonMock do
   def get(url, body \\ [], headers \\ [])
 
   def get(
+        "https://puckipedia.com/",
+        [Accept: "application/activity+json"],
+        []
+      ) do
+    {:ok,
+     %Response{
+       status_code: 200,
+       body: File.read!("test/fixtures/httpoison_mock/puckipedia.com.json")
+     }}
+  end
+
+  def get(
         "https://gerzilla.de/.well-known/webfinger?resource=acct:kaniini@gerzilla.de",
         [Accept: "application/xrd+xml,application/jrd+json"],
         follow_redirect: true
