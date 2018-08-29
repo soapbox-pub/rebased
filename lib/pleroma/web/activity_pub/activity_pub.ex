29b5e30c465ccb29465156d66eba02e6c0ef846f
William Pitcock <nenolod@dereferenced.org>
2018-08-29 18:38:30 +0000

    activity: drop recipients_to/recipients_cc fields

--- lib/pleroma/web/activity_pub/activity_pub.ex
+++ lib/pleroma/web/activity_pub/activity_pub.ex
@@ -60,16 +60,14 @@ defmodule Pleroma.Web.ActivityPub.ActivityPub do
          :ok <- check_actor_is_active(map["actor"]),
          {:ok, map} <- MRF.filter(map),
          :ok <- insert_full_object(map) do
-      {recipients, recipients_to, recipients_cc} = get_recipients(map)
+      {recipients, _, _} = get_recipients(map)
 
       {:ok, activity} =
         Repo.insert(%Activity{
           data: map,
           local: local,
           actor: map["actor"],
-          recipients: recipients,
-          recipients_to: recipients_to,
-          recipients_cc: recipients_cc
+          recipients: recipients
         })
 
       Notification.create_notifications(activity)
