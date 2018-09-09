a4fe14de026b38915a08c867fa9557c8d453e791
William Pitcock <nenolod@dereferenced.org>
2018-09-09 12:56:25 +0000

    tests: break the cycle using pleroma.factory

--- test/plugs/user_enabled_plug_test.exs
+++ test/plugs/user_enabled_plug_test.exs
@@ -2,6 +2,7 @@ defmodule Pleroma.Plugs.UserEnabledPlugTest do
   use Pleroma.Web.ConnCase, async: true
 
   alias Pleroma.Plugs.UserEnabledPlug
+  import Pleroma.Factory
 
   test "doesn't do anything if the user isn't set", %{conn: conn} do
     ret_conn =
@@ -12,18 +13,22 @@ defmodule Pleroma.Plugs.UserEnabledPlugTest do
   end
 
   test "with a user that is deactivated, it removes that user", %{conn: conn} do
+    user = insert(:user, info: %{"deactivated" => true})
+
     conn =
       conn
-      |> assign(:user, %Pleroma.User{info: %{"deactivated" => true}})
+      |> assign(:user, user)
       |> UserEnabledPlug.call(%{})
 
     assert conn.assigns.user == nil
   end
 
   test "with a user that is not deactivated, it does nothing", %{conn: conn} do
+    user = insert(:user)
+
     conn =
       conn
-      |> assign(:user, %Pleroma.User{})
+      |> assign(:user, user)
 
     ret_conn =
       conn
