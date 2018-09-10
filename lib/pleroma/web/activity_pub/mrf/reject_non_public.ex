e0b8c0ccba57cb8f920929c61b64c523f431edec
William Pitcock <nenolod@dereferenced.org>
2018-09-10 01:13:38 +0000

    MRF: reject non-public: use pattern match to remove unnecessary if block

--- lib/pleroma/web/activity_pub/mrf/reject_non_public.ex
+++ lib/pleroma/web/activity_pub/mrf/reject_non_public.ex
@@ -7,43 +7,42 @@ defmodule Pleroma.Web.ActivityPub.MRF.RejectNonPublic do
   @allow_direct Keyword.get(@mrf_rejectnonpublic, :allow_direct)
 
   @impl true
-  def filter(object) do
-    if object["type"] == "Create" do
-      user = User.get_cached_by_ap_id(object["actor"])
-      public = "https://www.w3.org/ns/activitystreams#Public"
+  def filter(%{"type" => "Create"} = object) do
+    user = User.get_cached_by_ap_id(object["actor"])
+    public = "https://www.w3.org/ns/activitystreams#Public"
 
-      # Determine visibility
-      visibility =
-        cond do
-          public in object["to"] -> "public"
-          public in object["cc"] -> "unlisted"
-          user.follower_address in object["to"] -> "followers"
-          true -> "direct"
+    # Determine visibility
+    visibility =
+      cond do
+        public in object["to"] -> "public"
+        public in object["cc"] -> "unlisted"
+        user.follower_address in object["to"] -> "followers"
+        true -> "direct"
+      end
+
+    case visibility do
+      "public" ->
+        {:ok, object}
+
+      "unlisted" ->
+        {:ok, object}
+
+      "followers" ->
+        with true <- @allow_followersonly do
+          {:ok, object}
+        else
+          _e -> {:reject, nil}
         end
 
-      case visibility do
-        "public" ->
+      "direct" ->
+        with true <- @allow_direct do
           {:ok, object}
-
-        "unlisted" ->
-          {:ok, object}
-
-        "followers" ->
-          with true <- @allow_followersonly do
-            {:ok, object}
-          else
-            _e -> {:reject, nil}
-          end
-
-        "direct" ->
-          with true <- @allow_direct do
-            {:ok, object}
-          else
-            _e -> {:reject, nil}
-          end
-      end
-    else
-      {:ok, object}
+        else
+          _e -> {:reject, nil}
+        end
     end
   end
+
+  @impl true
+  def filter(object), do: {:ok, object}
 end
