de0c1c641a1709cf393aecc3ef666d827ef5ebde
Haelwenn (lanodan) Monnier <contact@hacktivis.me>
2018-09-16 23:17:34 +0200

    [MastodonFE] Update Frontend (glitch)

---
+++ priv/static/packs/flavours/vanilla/common.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[84],{837:function(n,o,w){"use strict";w.r(o);w(838)},838:function(n,o,w){}},[[837,0]]]);
+//# sourceMappingURL=common.js.map
--- priv/static/packs/mastodon-light.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[35],{394:function(n,w,o){}},[[394,0]]]);
-//# sourceMappingURL=mastodon-light.js.map
