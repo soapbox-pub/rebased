fbb70d25fe28177b8d053d1055942abc7e7a3db9
lain <lain@soykaf.club>
2018-11-18 22:36:47 +0100

    Several twitter api fixes.

--- lib/pleroma/user/info.ex
+++ lib/pleroma/user/info.ex
@@ -4,6 +4,7 @@ defmodule Pleroma.User.Info do
 
   embedded_schema do
     field(:banner, :map, default: %{})
+    field(:background, :string, default: nil)
     field(:source_data, :map, default: %{})
     field(:note_count, :integer, default: 0)
     field(:follower_count, :integer, default: 0)
