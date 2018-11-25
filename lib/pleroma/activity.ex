5ea64f4bf958d9801d89d1165fa20d0e17bf0570
William Pitcock <nenolod@dereferenced.org>
2018-11-25 21:34:36 +0000

    activity: minor cleanups to normalization functions to align them with the object normalizers

--- lib/pleroma/activity.ex
+++ lib/pleroma/activity.ex
@@ -79,8 +79,8 @@ defmodule Pleroma.Activity do
 
   def get_create_activity_by_object_ap_id(_), do: nil
 
-  def normalize(obj) when is_map(obj), do: Activity.get_by_ap_id(obj["id"])
-  def normalize(ap_id) when is_binary(ap_id), do: Activity.get_by_ap_id(ap_id)
+  def normalize(obj) when is_map(obj), do: normalize(obj["id"])
+  def normalize(ap_id) when is_binary(ap_id), do: get_by_ap_id(ap_id)
   def normalize(_), do: nil
 
   defp get_in_reply_to_activity_from_object(%Object{data: %{"inReplyTo" => ap_id}}) do
