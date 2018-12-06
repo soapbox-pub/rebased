3d492795b76cfe3b3616607f815a0effe44c1ce5
Egor Kislitsyn <egor@kislitsyn.com>
2018-12-06 20:42:00 +0700

    clean up

--- lib/pleroma/web/mastodon_api/views/push_subscription_view.ex
+++ lib/pleroma/web/mastodon_api/views/push_subscription_view.ex
@@ -6,9 +6,7 @@ defmodule Pleroma.Web.MastodonAPI.PushSubscriptionView do
     %{
       id: to_string(subscription.id),
       endpoint: subscription.endpoint,
-      alerts: Map.get(subscription.data, "alerts"),
-      # TODO: generate VAPID server key
-      server_key: "N/A"
+      alerts: Map.get(subscription.data, "alerts")
     }
   end
 end
