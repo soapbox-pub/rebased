409ff60bf87d40919ec8cfcbe054cb66b87ca972
Karen Konou <konoukaren@gmail.com>
2018-12-22 23:32:38 +0100

    Fix formatting

--- lib/pleroma/web/activity_pub/mrf/hellthread_policy.ex
+++ lib/pleroma/web/activity_pub/mrf/hellthread_policy.ex
@@ -3,10 +3,9 @@ defmodule Pleroma.Web.ActivityPub.MRF.HellthreadPolicy do
 
   @impl true
   def filter(object) do
-
     policy = Pleroma.Config.get(:mrf_hellthreadmitigation)
 
-    if (length(object["to"]) + length(object["cc"])) > Keyword.get(policy, :threshold) do
+    if length(object["to"]) + length(object["cc"]) > Keyword.get(policy, :threshold) do
       {:reject, nil}
     else
       {:ok, object}
