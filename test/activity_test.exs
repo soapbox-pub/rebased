ab2ee436342c821b7def04a2b9f1f195c0437065
Maxim Filippov <colixer@gmail.com>
2018-12-25 03:41:14 +0300

    Fix Activity test

--- test/activity_test.exs
+++ test/activity_test.exs
@@ -5,14 +5,14 @@ defmodule Pleroma.ActivityTest do
 
   test "returns an activity by it's AP id" do
     activity = insert(:note_activity)
-    found_activity = Pleroma.Activity.get_by_ap_id(activity.data["id"])
+    found_activity = Activity.get_by_ap_id(activity.data["id"])
 
     assert activity == found_activity
   end
 
   test "returns activities by it's objects AP ids" do
     activity = insert(:note_activity)
-    [found_activity] = Pleroma.Activity.all_by_object_ap_id(activity.data["object"]["id"])
+    [found_activity] = Activity.all_by_object_ap_id(activity.data["object"]["id"])
 
     assert activity == found_activity
   end
@@ -21,7 +21,7 @@ defmodule Pleroma.ActivityTest do
     activity = insert(:note_activity)
 
     found_activity =
-      Pleroma.Activity.get_create_activity_by_object_ap_id(activity.data["object"]["id"])
+      Activity.get_create_activity_by_object_ap_id(activity.data["object"]["id"])
 
     assert activity == found_activity
   end
