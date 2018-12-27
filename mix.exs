58b12c09db96da4f3aafac60fc7e17517814a401
Ivan Tashkinov <ivantashkinov@gmail.com>
2018-12-27 16:12:33 +0300

    [Fixes #396] Required Elixir ~> 1.7.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Pleroma.Mixfile do
     [
       app: :pleroma,
       version: version("0.9.0"),
-      elixir: "~> 1.4",
+      elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       elixirc_options: [warnings_as_errors: true],
