f40562b4e1ff213b88c8b6edf57431bfb1b804ac
sxsdv1 <sxsdv1@gmail.com>
2018-12-30 11:19:53 +0100

    Respond with full activity to outbox post

--- lib/pleroma/web/activity_pub/activity_pub_controller.ex
+++ lib/pleroma/web/activity_pub/activity_pub_controller.ex
@@ -195,7 +195,7 @@ defmodule Pleroma.Web.ActivityPub.ActivityPubController do
         conn
         |> put_status(:created)
         |> put_resp_header("location", activity.data["id"])
-        |> json(%{"id" => activity.data["id"]})
+        |> json(activity.data)
       end
     else
       conn
