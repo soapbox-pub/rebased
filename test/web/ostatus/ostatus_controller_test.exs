8cea00e1dc2bf3c2e807b09fac886dfd7b4ea3e9
rinpatch <rinpatch@sdf.org>
2019-01-16 10:53:57 +0300

    Remove OpenGraph test from Ostatus tests

--- test/web/ostatus/ostatus_controller_test.exs
+++ test/web/ostatus/ostatus_controller_test.exs
@@ -156,19 +156,6 @@ defmodule Pleroma.Web.OStatus.OStatusControllerTest do
     |> response(404)
   end
 
-  test "renders notice metatags in html format", %{conn: conn} do
-    note_activity = insert(:note_activity)
-    conn = get(conn, "/notice/#{note_activity.id}")
-    body = html_response(conn, 200)
-    twitter_card_summary = "<meta content=\"summary\" property=\"twitter:card\">"
-
-    description_content =
-      "<meta content=\"#{note_activity.data["object"]["content"]}\" property=\"og:description\">"
-
-    assert body =~ twitter_card_summary
-    assert body =~ description_content
-  end
-
   test "gets a notice in xml format", %{conn: conn} do
     note_activity = insert(:note_activity)
 
