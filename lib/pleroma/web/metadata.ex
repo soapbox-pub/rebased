a76793006bb64723b4fe0aa054765edadc65905c
rinpatch <rinpatch@sdf.org>
2019-01-16 10:35:06 +0300

    Thanks you rinpatch, very cool

--- lib/pleroma/web/metadata.ex
+++ lib/pleroma/web/metadata.ex
@@ -2,10 +2,17 @@ defmodule Pleroma.Web.Metadata do
   alias Phoenix.HTML
 
   @parsers Pleroma.Config.get([:metadata, :providers], [])
+  def get_cached_tags(%{activity: activity, user: user} = params) do
+    # We don't need to use the both activity and a user since the object can't change it's content
+    key = "#{:erlang.term_to_binary(user)}#{activity.data["id"]}"
+
+    Cachex.fetch!(:metadata_cache, key, fn _key ->
+      {:commit, build_tags(params)}
+    end)
+  end
 
   def get_cached_tags(%{user: user} = params) do
     # I am unsure how well ETS works with big keys
-    # We don't need to use the both activity and a user since the object can't change it's content
     key = :erlang.term_to_binary(user)
 
     Cachex.fetch!(:metadata_cache, key, fn _key ->
