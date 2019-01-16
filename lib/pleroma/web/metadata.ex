293f6a8b712246d0580f9eb113c798ae1ea3b634
rinpatch <rinpatch@sdf.org>
2019-01-16 17:15:13 +0300

    join us now and share the software~. Also tests

--- lib/pleroma/web/metadata.ex
+++ lib/pleroma/web/metadata.ex
@@ -1,3 +1,6 @@
+# Pleroma: A lightweight social networking server
+# Copyright © 2017-2019 Pleroma Authors <https://pleroma.social/>
+# SPDX-License-Identifier: AGPL-3.0-only
 defmodule Pleroma.Web.Metadata do
   alias Phoenix.HTML
 
