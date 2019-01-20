3dadaa4432b442d75b0ac0425aa05527d52f0e7a
William Pearson <uiri00@gmail.com>
2019-01-20 01:44:00 +0000

    robots.txt
    Add default robots.txt that allows bots access to all paths.
    Add mix task to generate robots.txt taht allows bots access to no paths.
    Document custom emojis, MRF and static_dir
    static_dir documentation includes docs for the robots.txt Mix task.

--- lib/pleroma/web/endpoint.ex
+++ lib/pleroma/web/endpoint.ex
@@ -25,7 +25,7 @@ defmodule Pleroma.Web.Endpoint do
     at: "/",
     from: :pleroma,
     only:
-      ~w(index.html static finmoji emoji packs sounds images instance sw.js sw-pleroma.js favicon.png schemas doc)
+      ~w(index.html robots.txt static finmoji emoji packs sounds images instance sw.js sw-pleroma.js favicon.png schemas doc)
     # credo:disable-for-previous-line Credo.Check.Readability.MaxLineLength
   )
 
