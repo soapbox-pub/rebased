e10cda7541f5d76a32d0bf27d90a51c5fc8e7fcf
Karen Konou <konoukaren@gmail.com>
2019-02-03 22:46:06 +0100

    implemented tweaks

--- lib/pleroma/web/activity_pub/mrf/hellthread_policy.ex
+++ lib/pleroma/web/activity_pub/mrf/hellthread_policy.ex
@@ -6,27 +6,34 @@ defmodule Pleroma.Web.ActivityPub.MRF.HellthreadPolicy do
   alias Pleroma.User
   @behaviour Pleroma.Web.ActivityPub.MRF
 
+  defp delist_message(object) do
+    follower_collection = User.get_by_ap_id(object["actor"].follower_address)
+
+    object
+    |> Kernel.update_in(["to"], [follower_collection])
+    |> Kernel.update_in(["cc"], ["https://www.w3.org/ns/activitystreams#Public"])
+  end
+
   @impl true
   def filter(%{"type" => "Create"} = object) do
     delist_threshold = Pleroma.Config.get([:mrf_hellthread, :delist_threshold])
-    reject_threshold = Pleroma.Config.get([:mrf_hellthread, :reject_threshold])
+
+    reject_threshold =
+      Pleroma.Config.get(
+        [:mrf_hellthread, :reject_threshold],
+        Pleroma.Config.get([:mrf_hellthread, :threshold])
+      )
+
     recipients = (object["to"] || []) ++ (object["cc"] || [])
 
     cond do
-      length(recipients) > reject_threshold and reject_threshold != 0 ->
+      length(recipients) > reject_threshold and reject_threshold > 0 ->
         {:reject, nil}
 
-      length(recipients) > delist_threshold and delist_threshold != 0 ->
+      length(recipients) > delist_threshold and delist_threshold > 0 ->
         if Enum.member?(object["to"], "https://www.w3.org/ns/activitystreams#Public") or
-             Enum.member?(object["cc"], "https://www.w3.org/ns/activitystreams#Public") do
-          follower_collection = User.get_by_ap_id(object["actor"].follower_address)
-
-          object
-          |> Kernel.update_in(["object", "to"], [follower_collection])
-          |> Kernel.update_in(["object", "cc"], ["https://www.w3.org/ns/activitystreams#Public"])
-          |> Kernel.update_in(["to"], [follower_collection])
-          |> Kernel.update_in(["cc"], ["https://www.w3.org/ns/activitystreams#Public"])
-
+             Enum.member?(object["to"], "https://www.w3.org/ns/activitystreams#Public") do
+          delist_message(object)
           {:ok, object}
         else
           {:ok, object}
