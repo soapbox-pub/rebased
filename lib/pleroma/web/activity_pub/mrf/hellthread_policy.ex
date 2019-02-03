0ef0ae35abf7c1f1016175bd446436f9e5dd8fc2
Karen Konou <konoukaren@gmail.com>
2019-02-03 20:12:23 +0100

    added optional delist feature

--- lib/pleroma/web/activity_pub/mrf/hellthread_policy.ex
+++ lib/pleroma/web/activity_pub/mrf/hellthread_policy.ex
@@ -3,17 +3,37 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 
 defmodule Pleroma.Web.ActivityPub.MRF.HellthreadPolicy do
+  alias Pleroma.User
   @behaviour Pleroma.Web.ActivityPub.MRF
 
   @impl true
   def filter(%{"type" => "Create"} = object) do
-    threshold = Pleroma.Config.get([:mrf_hellthread, :threshold])
+    delist_threshold = Pleroma.Config.get([:mrf_hellthread, :delist_threshold])
+    reject_threshold = Pleroma.Config.get([:mrf_hellthread, :reject_threshold])
     recipients = (object["to"] || []) ++ (object["cc"] || [])
 
-    if length(recipients) > threshold do
-      {:reject, nil}
-    else
-      {:ok, object}
+    cond do
+      length(recipients) > reject_threshold ->
+        {:reject, nil}
+
+      length(recipients) > delist_threshold ->
+        if Enum.member?(object["to"], "https://www.w3.org/ns/activitystreams#Public") or
+             Enum.member?(object["cc"], "https://www.w3.org/ns/activitystreams#Public") do
+          object
+          |> Kernel.update_in(["object", "to"], [
+            User.get_cached_by_ap_id(object["actor"].follower_address)
+          ])
+          |> Kernel.update_in(["object", "cc"], ["https://www.w3.org/ns/activitystreams#Public"])
+          |> Kernel.update_in(["to"], [
+            User.get_cached_by_ap_id(object["actor"].follower_address)
+          ])
+          |> Kernel.update_in(["cc"], ["https://www.w3.org/ns/activitystreams#Public"])
+        else
+          {:ok, object}
+        end
+
+      true ->
+        {:ok, object}
     end
   end
 
