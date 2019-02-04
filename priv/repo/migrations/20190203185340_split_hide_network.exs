cd6606b7f69d1604a08cb31fa539cbd549591e23
Maxim Filippov <colixer@gmail.com>
2019-02-04 22:23:09 +0300

    Initialize hide_network with false

--- priv/repo/migrations/20190203185340_split_hide_network.exs
+++ priv/repo/migrations/20190203185340_split_hide_network.exs
@@ -2,6 +2,7 @@ defmodule Pleroma.Repo.Migrations.SplitHideNetwork do
   use Ecto.Migration
 
   def up do
+    execute("UPDATE users SET info = '{\"hide_network\": false}'::jsonb WHERE NOT(info::jsonb ? 'hide_network')")
     execute("UPDATE users SET info = jsonb_set(info, '{hide_followings}'::text[], info->'hide_network')")
     execute("UPDATE users SET info = jsonb_set(info, '{hide_followers}'::text[], info->'hide_network')")
   end
