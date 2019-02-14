949e35e26deaf6dc7c2e552b1b8db63de8a87445
Ivan Tashkinov <ivantashkinov@gmail.com>
2019-02-14 14:28:26 +0300

    [#468] OAuth scopes-related data migration simplification.

---
+++ priv/repo/migrations/20190213185600_data_migration_populate_o_auth_scopes.exs
@@ -0,0 +1,11 @@
+defmodule Pleroma.Repo.Migrations.DataMigrationPopulateOAuthScopes do
+  use Ecto.Migration
+
+  def up do
+    for t <- [:oauth_authorizations, :oauth_tokens] do
+      execute "UPDATE #{t} SET scopes = apps.scopes FROM apps WHERE #{t}.app_id = apps.id;"
+    end
+  end
+
+  def down, do: :noop
+end
