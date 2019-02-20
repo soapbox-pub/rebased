840d2c9f0bcc493703d12409bc64d2931b9ee3f5
lain <lain@soykaf.club>
2019-02-20 13:47:24 +0100

    Remove superfluous migration.

--- priv/repo/migrations/20190219192317_create_user_mutes.exs
+++
@@ -1,7 +0,0 @@
-defmodule Pleroma.Repo.Migrations.CreateUserMutes do
-  use Ecto.Migration
-
-  def change do
-    execute "UPDATE users SET info = jsonb_set(info, '{mutes}', '[]'::jsonb);"
-  end
-end
