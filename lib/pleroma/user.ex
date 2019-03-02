39a5bea9b73daa63203b0640d0a7906d6f1af143
Haelwenn (lanodan) Monnier <contact@hacktivis.me>
2019-03-02 06:57:28 +0100

    Pleroma.User: Fix syntax and user.ap_id call

--- lib/pleroma/user.ex
+++ lib/pleroma/user.ex
@@ -1204,13 +1204,13 @@ defmodule Pleroma.User do
       end)
 
     # TODO: get profile URLs other than user.ap_id
-    profile_urls = [user[:ap_id]]
+    profile_urls = [user.ap_id]
 
     bio
-    |> CommonUtils.format_input("text/plain", %{
+    |> CommonUtils.format_input("text/plain", [
       mentions_format: :full,
       rel: &RelMe.maybe_put_rel_me(&1, profile_urls)
-    })
+    ])
     |> elem(0)
     |> Formatter.emojify(emoji)
   end
