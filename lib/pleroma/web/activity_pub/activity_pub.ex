6a69ece437e4c2b503b8ec8cd5de2a4a63519f02
Karen Konou <konoukaren@gmail.com>
2019-03-07 11:53:15 +0100

    Revert existing object check

--- lib/pleroma/web/activity_pub/activity_pub.ex
+++ lib/pleroma/web/activity_pub/activity_pub.ex
@@ -311,14 +311,8 @@ defmodule Pleroma.Web.ActivityPub.ActivityPub do
     user = User.get_cached_by_ap_id(actor)
 
     to =
-      case Object.get_cached_by_ap_id(id) do
-        nil ->
-          [user.follower_address, "https://www.w3.org/ns/activitystreams#Public"]
-
-        object ->
-          object.data["to"] || [] ++ object.data["cc"] ||
-            [] ++ [user.follower_address, "https://www.w3.org/ns/activitystreams#Public"]
-      end
+      object.data["to"] || [] ++ object.data["cc"] ||
+        [] ++ [user.follower_address, "https://www.w3.org/ns/activitystreams#Public"]
 
     data = %{
       "type" => "Delete",
