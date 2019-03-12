faf238c1b0b4d814ce3b2e041ed6b18b498233bf
rinpatch <rinpatch@sdf.org>
2019-03-12 15:33:15 +0300

    Fix upload tests

--- test/upload_test.exs
+++ test/upload_test.exs
@@ -56,7 +56,7 @@ defmodule Pleroma.UploadTest do
 
       assert List.first(data["url"])["href"] ==
                Pleroma.Web.base_url() <>
-                 "/media/e7a6d0cf595bff76f14c9a98b6c199539559e8b844e02e51e5efcfd1f614a2df.jpg"
+                 "/media/e7a6d0cf595bff76f14c9a98b6c199539559e8b844e02e51e5efcfd1f614a2df.jpg?name=an%20%5Bimage.jpg"
     end
 
     test "copies the file to the configured folder without deduping" do
@@ -150,7 +150,8 @@ defmodule Pleroma.UploadTest do
       {:ok, data} = Upload.store(file)
       [attachment_url | _] = data["url"]
 
-      assert Path.basename(attachment_url["href"]) == "an%E2%80%A6%20image.jpg"
+      assert Path.basename(attachment_url["href"]) ==
+               "an%E2%80%A6%20image.jpg?name=an%E2%80%A6%20image.jpg"
     end
 
     test "escapes reserved uri characters" do
@@ -166,7 +167,7 @@ defmodule Pleroma.UploadTest do
       [attachment_url | _] = data["url"]
 
       assert Path.basename(attachment_url["href"]) ==
-               "%3A%3F%23%5B%5D%40%21%24%26%5C%27%28%29%2A%2B%2C%3B%3D.jpg"
+               "%3A%3F%23%5B%5D%40%21%24%26%5C%27%28%29%2A%2B%2C%3B%3D.jpg?name=%3A%3F%23%5B%5D%40%21%24%26%5C%27%28%29%2A%2B%2C%3B%3D.jpg"
     end
   end
 end
