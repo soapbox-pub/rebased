07afb9b537fd8966ceefda74f978cf7cfd2c09ea
Sachin Joshi <satchin.joshi@gmail.com>
2019-04-08 15:16:18 +0545

    update the follower count when a follower is blocked

--- lib/pleroma/user.ex
+++ lib/pleroma/user.ex
@@ -937,6 +937,8 @@ defmodule Pleroma.User do
       unfollow(blocked, blocker)
     end
 
+    {:ok, blocker} = update_follower_count(blocker)
+
     info_cng =
       blocker.info
       |> User.Info.add_to_block(ap_id)
