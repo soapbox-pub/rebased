51c97d71af7788134d4af13e0f09d4ed3aa95c82
Haelwenn (lanodan) Monnier <contact@hacktivis.me>
2019-04-11 07:29:03 +0200

    Update Mastodon Frontend to 2019-04-11 bundle
    See: https://git.pleroma.social/pleroma/mastofe/tags/bundle-2019-04-11
    
    [ci skip]

--- priv/static/packs/flavours/vanilla/common.js
+++ priv/static/packs/flavours/vanilla/common.js
@@ -1,2 +1,2 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[86],{845:function(n,o,w){"use strict";w.r(o);w(846)},846:function(n,o,w){}},[[845,0]]]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[86],{839:function(n,o,w){"use strict";w.r(o);w(840)},840:function(n,o,w){}},[[839,0]]]);
 //# sourceMappingURL=common.js.map
--- priv/static/packs/skins/glitch/contrast/common.js
+++ priv/static/packs/skins/glitch/contrast/common.js
@@ -1,2 +1,2 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[181],{842:function(n,w,o){}},[[842,0]]]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[182],{836:function(n,w,o){}},[[836,0]]]);
 //# sourceMappingURL=common.js.map
