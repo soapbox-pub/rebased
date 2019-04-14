ec42b639a347903ea5c0e5f9b365f1d5ea9e624f
Sadposter <hannah+pleroma@coffee-and-dreams.uk>
2019-04-14 15:46:54 +0100

    fix pattern match on user registration
    deliver_async will return a single atom

--- lib/pleroma/user.ex
+++ lib/pleroma/user.ex
@@ -270,7 +270,7 @@ defmodule Pleroma.User do
     with {:ok, user} <- Repo.insert(changeset),
          {:ok, user} <- autofollow_users(user),
          {:ok, _} <- Pleroma.User.WelcomeMessage.post_welcome_message_to_user(user),
-         {:ok, _} <- try_send_confirmation_email(user) do
+         :ok <- try_send_confirmation_email(user) do
       {:ok, user}
     end
   end
