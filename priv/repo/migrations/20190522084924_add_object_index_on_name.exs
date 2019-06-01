67bcc3ccc433bd5c7b59f5bf1a45db93da93d093
rinpatch <rinpatch@sdf.org>
2019-06-01 21:49:18 +0300

    Remove index on object name used for excluding poll votes (replaced by just rewriting Note to Answer)

--- priv/repo/migrations/20190522084924_add_object_index_on_name.exs
+++
@@ -1,7 +0,0 @@
-defmodule Pleroma.Repo.Migrations.AddObjectIndexOnName do
-  use Ecto.Migration
-
-  def change do
-    create(index(:objects, ["(data->'name')"], name: :objects_name_index, using: :gin))
-  end
-end
