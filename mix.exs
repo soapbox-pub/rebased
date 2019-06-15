2ad5a8511d93e1e2b30a4798998393c61e981fa5
Mark Felder <feld@FreeBSD.org>
2019-06-15 07:03:26 -0500

    Update Phoenix to 1.4.8

--- mix.exs
+++ mix.exs
@@ -85,7 +85,7 @@ defmodule Pleroma.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.1"},
+      {:phoenix, "~> 1.4.8"},
       {:plug_cowboy, "~> 2.0"},
       {:phoenix_pubsub, "~> 1.1"},
       {:phoenix_ecto, "~> 4.0"},
