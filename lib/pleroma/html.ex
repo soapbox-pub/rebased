035368d363e31bd99efb21e1c121574718c81b5e
rinpatch <rinpatch@sdf.org>
2019-06-19 00:31:30 +0300

    Rich Media: Skip Microformats hashtags
    When fixing this problem I incorrectly assumed a.hashtag is
    the proper way for detecting hashtags, but it is just something Pleroma and
    Mastodon add. Per microformats it should be detected by the presense of rel=tag.
    
    This MR adds a check for rel=tag, but I still left a.hashtag just in case

--- lib/pleroma/html.ex
+++ lib/pleroma/html.ex
@@ -89,7 +89,7 @@ defmodule Pleroma.HTML do
     Cachex.fetch!(:scrubber_cache, key, fn _key ->
       result =
         content
-        |> Floki.filter_out("a.mention,a.hashtag")
+        |> Floki.filter_out("a.mention,a.hashtag,a[rel~=\"tag\"]")
         |> Floki.attribute("a", "href")
         |> Enum.at(0)
 
