291d95dcc3cd85050421d2ab022e01a6d89c0f3d
Roman Chvanikov <chvanikoff@pm.me>
2019-07-03 01:14:40 +0300

    Fix migration

--- priv/repo/migrations/20170522160642_case_insensivtivity.exs
+++ priv/repo/migrations/20170522160642_case_insensivtivity.exs
@@ -2,18 +2,24 @@ defmodule Pleroma.Repo.Migrations.CaseInsensivtivity do
   use Ecto.Migration
 
   def up do
-    execute ("create extension if not exists citext")
+    execute("create extension if not exists citext")
+
+    drop_if_exists(index(:users, [:email]))
+
     alter table(:users) do
-      modify :email, :citext
-      modify :nickname, :citext
+      modify(:email, :citext)
+      modify(:nickname, :citext)
     end
+
+    create_if_not_exists(index(:users, [:email]))
   end
 
   def down do
     alter table(:users) do
-      modify :email, :string
-      modify :nickname, :string
+      modify(:email, :string)
+      modify(:nickname, :string)
     end
-    execute ("drop extension if exists citext")
+
+    execute("drop extension if exists citext")
   end
 end
