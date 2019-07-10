93a0eeab16dc98b9278ee8649b233c3acd7807ec
feld <feld@feld.me>
2019-07-10 05:13:23 +0000

    Add license/copyright to all project files

--- lib/pleroma/config/transfer_task.ex
+++ lib/pleroma/config/transfer_task.ex
@@ -1,3 +1,7 @@
+# Pleroma: A lightweight social networking server
+# Copyright © 2017-2019 Pleroma Authors <https://pleroma.social/>
+# SPDX-License-Identifier: AGPL-3.0-only
+
 defmodule Pleroma.Config.TransferTask do
   use Task
   alias Pleroma.Web.AdminAPI.Config
--- lib/pleroma/web/metadata/rel_me.ex
+++ lib/pleroma/web/metadata/rel_me.ex
@@ -1,3 +1,7 @@
+# Pleroma: A lightweight social networking server
+# Copyright © 2017-2019 Pleroma Authors <https://pleroma.social/>
+# SPDX-License-Identifier: AGPL-3.0-only
+
 defmodule Pleroma.Web.Metadata.Providers.RelMe do
   alias Pleroma.Web.Metadata.Providers.Provider
   @behaviour Provider
