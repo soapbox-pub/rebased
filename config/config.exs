fa7e0c4262f8844bb6224c200f7d41720607fcac
rinpatch <rinpatch@sdf.org>
2019-07-12 23:53:21 +0300

    Workaround for remote server certificate chain issues

--- config/config.exs
+++ config/config.exs
@@ -194,6 +194,8 @@ config :pleroma, :http,
   send_user_agent: true,
   adapter: [
     ssl_options: [
+      # Workaround for remote server certificate chain issues
+      partial_chain: &:hackney_connect.partial_chain/1,
       # We don't support TLS v1.3 yet
       versions: [:tlsv1, :"tlsv1.1", :"tlsv1.2"]
     ]
