d302fcaa6181cb6e2df890b724d871cb86d1e27b
lain <lain@soykaf.club>
2019-07-15 16:47:39 +0900

    Dependencies: Update tzdata and related packages.

--- mix.exs
+++ mix.exs
@@ -95,6 +95,7 @@ defmodule Pleroma.Mixfile do
   defp deps do
     [
       {:phoenix, "~> 1.4.8"},
+      {:tzdata, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:phoenix_pubsub, "~> 1.1"},
       {:phoenix_ecto, "~> 4.0"},
