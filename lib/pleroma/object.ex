c30cc039e423e8f31d0222747e301514b7d0dd9e
lain <lain@soykaf.club>
2019-08-27 12:22:30 -0500

    Transmogrifier: Use Containment.get_actor to get actors.

--- lib/pleroma/object.ex
+++ lib/pleroma/object.ex
@@ -230,8 +230,4 @@ defmodule Pleroma.Object do
       _ -> :noop
     end
   end
-
-  def get_ap_id(%{"id" => id}), do: id
-  def get_ap_id(id) when is_binary(id), do: id
-  def get_ap_id(_), do: {:error, "Object is not a string and has no id."}
 end
