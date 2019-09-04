c2b6c1b089a813cf8c7cbd54c0f80bee4985522c
rinpatch <rinpatch@sdf.org>
2019-09-04 11:33:08 +0300

    Extend `/api/pleroma/notifications/read` to mark multiple notifications as read and make it respond with Mastoapi entities

--- lib/pleroma/web/router.ex
+++ lib/pleroma/web/router.ex
@@ -236,12 +236,6 @@ defmodule Pleroma.Web.Router do
       post("/blocks_import", UtilController, :blocks_import)
       post("/follow_import", UtilController, :follow_import)
     end
-
-    scope [] do
-      pipe_through(:oauth_read)
-
-      post("/notifications/read", UtilController, :notifications_read)
-    end
   end
 
   scope "/oauth", Pleroma.Web.OAuth do
@@ -277,6 +271,7 @@ defmodule Pleroma.Web.Router do
     scope [] do
       pipe_through(:oauth_write)
       patch("/conversations/:id", PleromaAPIController, :update_conversation)
+      post("/notifications/read", PleromaAPIController, :read_notification)
     end
   end
 
