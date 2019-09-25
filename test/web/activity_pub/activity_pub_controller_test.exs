d4a76b0a6ffba70c0f0bc10a4175cfdd99f8d574
rinpatch <rinpatch@sdf.org>
2019-09-25 15:59:04 +0300

    Don't embed the first page in inboxes/outboxes and refactor the views to follow View/Controller pattern
    Note that I mentioned the change in 1.1 section because I intend to
    backport this, if this is not needed I will move it back to Unreleased.

--- test/web/activity_pub/activity_pub_controller_test.exs
+++ test/web/activity_pub/activity_pub_controller_test.exs
@@ -473,7 +473,7 @@ defmodule Pleroma.Web.ActivityPub.ActivityPubControllerTest do
         conn
         |> assign(:user, user)
         |> put_req_header("accept", "application/activity+json")
-        |> get("/users/#{user.nickname}/inbox")
+        |> get("/users/#{user.nickname}/inbox?page=true")
 
       assert response(conn, 200) =~ note_object.data["content"]
     end
@@ -559,7 +559,7 @@ defmodule Pleroma.Web.ActivityPub.ActivityPubControllerTest do
       conn =
         conn
         |> put_req_header("accept", "application/activity+json")
-        |> get("/users/#{user.nickname}/outbox")
+        |> get("/users/#{user.nickname}/outbox?page=true")
 
       assert response(conn, 200) =~ note_object.data["content"]
     end
@@ -571,7 +571,7 @@ defmodule Pleroma.Web.ActivityPub.ActivityPubControllerTest do
       conn =
         conn
         |> put_req_header("accept", "application/activity+json")
-        |> get("/users/#{user.nickname}/outbox")
+        |> get("/users/#{user.nickname}/outbox?page=true")
 
       assert response(conn, 200) =~ announce_activity.data["object"]
     end
