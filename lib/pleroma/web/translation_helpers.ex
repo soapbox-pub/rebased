0f9c2c8b87672517aa040a2cbe1c297b29acc317
Maxim Filippov <colixer@gmail.com>
2019-10-01 18:10:04 +0300

    Send an identifier alongside with error message in OAuthController

--- lib/pleroma/web/translation_helpers.ex
+++ lib/pleroma/web/translation_helpers.ex
@@ -3,14 +3,21 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 
 defmodule Pleroma.Web.TranslationHelpers do
-  defmacro render_error(conn, status, msgid, bindings \\ Macro.escape(%{})) do
+  defmacro render_error(
+             conn,
+             status,
+             msgid,
+             bindings \\ Macro.escape(%{}),
+             identifier \\ Macro.escape("")
+           ) do
     quote do
       require Pleroma.Web.Gettext
 
       unquote(conn)
       |> Plug.Conn.put_status(unquote(status))
       |> Phoenix.Controller.json(%{
-        error: Pleroma.Web.Gettext.dgettext("errors", unquote(msgid), unquote(bindings))
+        error: Pleroma.Web.Gettext.dgettext("errors", unquote(msgid), unquote(bindings)),
+        identifier: unquote(identifier)
       })
     end
   end
