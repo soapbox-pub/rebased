e442180d111258a0131aaef0e84e621518462455
rinpatch <rinpatch@sdf.org>
2019-10-06 17:53:03 +0300

    Fix muted notification migration only working with a schema
    Should also improve performance. I tested it on my local DB, but if
    anyone has a backup of <=1.0 db they can test this on, please do

--- priv/repo/migrations/20190711042024_copy_muted_to_muted_notifications.exs
+++ priv/repo/migrations/20190711042024_copy_muted_to_muted_notifications.exs
@@ -1,19 +1,8 @@
 defmodule Pleroma.Repo.Migrations.CopyMutedToMutedNotifications do
   use Ecto.Migration
-  import Ecto.Query
   alias Pleroma.User
 
   def change do
-    query = from(u in "users", where: fragment("not (?->'deactivated' @> 'true')", u.info), select: %{info: u.info}, where: u.local == true, order_by: u.id)
-    Pleroma.Repo.stream(query)
-    |> Enum.each(fn
-      %{info: %{mutes: mutes} = info} = user ->
-        info_cng =
-          Ecto.Changeset.cast(info, %{muted_notifications: mutes}, [:muted_notifications])
-
-        Ecto.Changeset.change(user)
-        |> Ecto.Changeset.put_embed(:info, info_cng)
-        |> Pleroma.Repo.update()
-    end)
+  execute("update users set info = jsonb_set(info, '{muted_notifications}', info->'mutes', true) where local = true")
   end
 end
