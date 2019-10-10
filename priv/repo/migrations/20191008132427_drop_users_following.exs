2c7ff32e5b95926af9b6573a6dc6ea96e3ba7dd5
Egor Kislitsyn <egor@kislitsyn.com>
2019-10-10 21:11:34 +0700

    Add `thread_visibility` to migrations

--- priv/repo/migrations/20191008132427_drop_users_following.exs
+++ priv/repo/migrations/20191008132427_drop_users_following.exs
@@ -1,6 +1,8 @@
 defmodule Pleroma.Repo.Migrations.DropUsersFollowing do
   use Ecto.Migration
 
+  # had to disable these to be able to restore `following` index concurrently
+  # https://hexdocs.pm/ecto_sql/Ecto.Migration.html#index/3-adding-dropping-indexes-concurrently
   @disable_ddl_transaction true
   @disable_migration_lock true
 
