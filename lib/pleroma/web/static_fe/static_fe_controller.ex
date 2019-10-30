33a26b61c30ad8084003f0f1c646bc997a8d88ac
Phil Hagelberg <phil@hagelb.org>
2019-10-29 20:30:43 -0700

    Remove activity/user representer; move logic to controller.

--- lib/pleroma/web/static_fe/static_fe_controller.ex
+++ lib/pleroma/web/static_fe/static_fe_controller.ex
@@ -5,32 +5,65 @@
 defmodule Pleroma.Web.StaticFE.StaticFEController do
   use Pleroma.Web, :controller
 
+  alias Pleroma.Activity
+  alias Pleroma.Object
+  alias Pleroma.User
   alias Pleroma.Web.ActivityPub.ActivityPub
-  alias Pleroma.Web.StaticFE.ActivityRepresenter
-  alias Pleroma.Web.StaticFE.UserRepresenter
+  alias Pleroma.Web.Router.Helpers
 
   plug(:put_layout, :static_fe)
   plug(:put_view, Pleroma.Web.StaticFE.StaticFEView)
   plug(:assign_id)
   action_fallback(:not_found)
 
+  defp get_title(%Object{data: %{"name" => name}}) when is_binary(name),
+    do: name
+
+  defp get_title(%Object{data: %{"summary" => summary}}) when is_binary(summary),
+    do: summary
+
+  defp get_title(_), do: nil
+
+  def represent(%Activity{} = activity, %User{} = user, selected) do
+    %{
+      user: user,
+      title: get_title(activity.object),
+      content: activity.object.data["content"] || nil,
+      attachment: activity.object.data["attachment"],
+      link: Helpers.o_status_url(Pleroma.Web.Endpoint, :notice, activity.id),
+      published: activity.object.data["published"],
+      sensitive: activity.object.data["sensitive"],
+      selected: selected
+    }
+  end
+
+  def represent(%Activity{} = activity, selected) do
+    {:ok, user} = User.get_or_fetch(activity.data["actor"])
+    represent(activity, user, selected)
+  end
+
   def show_notice(%{assigns: %{notice_id: notice_id}} = conn, _params) do
-    with {:ok, data} <- ActivityRepresenter.represent(notice_id) do
-      context = data.object.data["context"]
+    activity = Activity.get_by_id_with_object(notice_id)
+    context = activity.object.data["context"]
+    activities = ActivityPub.fetch_activities_for_context(context, %{})
 
-      activities =
-        for a <- Enum.reverse(ActivityPub.fetch_activities_for_context(context, %{})) do
-          ActivityRepresenter.prepare_activity(data.user, a)
-          |> Map.put(:selected, a.object.id == data.object.id)
-        end
+    represented =
+      for a <- Enum.reverse(activities) do
+        represent(activity, a.object.id == activity.object.id)
+      end
 
-      render(conn, "conversation.html", activities: activities)
-    end
+    render(conn, "conversation.html", activities: represented)
   end
 
   def show_user(%{assigns: %{username_or_id: username_or_id}} = conn, _params) do
-    {:ok, data} = UserRepresenter.represent(username_or_id)
-    render(conn, "profile.html", %{user: data.user, timeline: data.timeline})
+    %User{} = user = User.get_cached_by_nickname_or_id(username_or_id)
+
+    timeline =
+      for activity <- ActivityPub.fetch_user_activities(user, nil, %{}) do
+        represent(activity, user, false)
+      end
+
+    render(conn, "profile.html", %{user: user, timeline: timeline})
   end
 
   def assign_id(%{path_info: ["notice", notice_id]} = conn, _opts),
