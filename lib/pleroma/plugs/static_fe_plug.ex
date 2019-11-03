e8bee35578fbbc442657baa4dee0047906b247a9
Phil Hagelberg <phil@hagelb.org>
2019-11-03 12:29:17 -0800

    Static FE plug should only respond to text/html requests.

--- lib/pleroma/plugs/static_fe_plug.ex
+++ lib/pleroma/plugs/static_fe_plug.ex
@@ -5,9 +5,14 @@
 defmodule Pleroma.Plugs.StaticFEPlug do
   def init(options), do: options
 
+  def accepts_html?({"accept", a}), do: String.contains?(a, "text/html")
+  def accepts_html?({_, _}), do: false
+
   def call(conn, _) do
-    case Pleroma.Config.get([:instance, :static_fe], false) do
-      true -> Pleroma.Web.StaticFE.StaticFEController.call(conn, :show)
+    with true <- Pleroma.Config.get([:instance, :static_fe], false),
+         {_, _} <- Enum.find(conn.req_headers, &accepts_html?/1) do
+      Pleroma.Web.StaticFE.StaticFEController.call(conn, :show)
+    else
       _ -> conn
     end
   end
