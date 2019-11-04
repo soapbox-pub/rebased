4388255eed40c436d5931c3d0d070f3c1c9aab83
lain <lain@soykaf.club>
2019-11-04 18:13:37 +0100

    Migrations: Don't copy over mutes unless there actually are some.

--- priv/repo/migrations/20190711042024_copy_muted_to_muted_notifications.exs
+++ priv/repo/migrations/20190711042024_copy_muted_to_muted_notifications.exs
@@ -3,6 +3,6 @@ defmodule Pleroma.Repo.Migrations.CopyMutedToMutedNotifications do
   alias Pleroma.User
 
   def change do
-  execute("update users set info = safe_jsonb_set(info, '{muted_notifications}', info->'mutes', true) where local = true")
+  execute("update users set info = safe_jsonb_set(info, '{muted_notifications}', info->'mutes', true) where local = true and info->'mutes' is not null")
   end
 end
