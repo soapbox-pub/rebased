89ca9560e092b3bc22ec94b98c79b21c238e1628
rinpatch <rinpatch@sdf.org>
2019-11-05 12:37:25 +0300

    Bump HtmlEntities to 0.5
    This release brings a major performance imrovement, see
    https://github.com/martinsvalin/html_entities/pull/17

--- mix.exs
+++ mix.exs
@@ -106,7 +106,7 @@ defmodule Pleroma.Mixfile do
       {:pbkdf2_elixir, "~> 0.12.3"},
       {:trailing_format_plug, "~> 0.0.7"},
       {:html_sanitize_ex, "~> 1.3.0"},
-      {:html_entities, "~> 0.4"},
+      {:html_entities, "~> 0.5", override: true},
       {:phoenix_html, "~> 2.10"},
       {:calendar, "~> 0.17.4"},
       {:cachex, "~> 3.0.2"},
