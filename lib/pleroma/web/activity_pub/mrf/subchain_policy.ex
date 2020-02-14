478619782b5f851e88ae2677cd17e528274c8f07
Mark Felder <feld@FreeBSD.org>
2020-02-14 11:02:24 -0600

    Standardize @behaviour usage in MRFs

--- lib/pleroma/web/activity_pub/mrf/subchain_policy.ex
+++ lib/pleroma/web/activity_pub/mrf/subchain_policy.ex
@@ -8,7 +8,7 @@ defmodule Pleroma.Web.ActivityPub.MRF.SubchainPolicy do
 
   require Logger
 
-  @behaviour MRF
+  @behaviour Pleroma.Web.ActivityPub.MRF
 
   defp lookup_subchain(actor) do
     with matches <- Config.get([:mrf_subchain, :match_actor]),
