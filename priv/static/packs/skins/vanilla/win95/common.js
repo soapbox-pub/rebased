2a850abbeb70f988e5e78bcd2c6bec42b1b6282c
Haelwenn (lanodan) Monnier <contact@hacktivis.me>
2020-02-29 06:41:13 +0100

    MastoFE: Bundle 2020-01-20

--- priv/static/packs/skins/vanilla/win95/common.js
+++ priv/static/packs/skins/vanilla/win95/common.js
@@ -1,2 +1,2 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[201],{918:function(n,w,o){}},[[918,0]]]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[248],{941:function(n,w,o){}},[[941,0]]]);
 //# sourceMappingURL=common.js.map
