05da5f5cca00c8b061858ff85e4a4640f3d91cb6
Mark Felder <feld@FreeBSD.org>
2020-03-03 16:44:49 -0600

    Update Copyrights

--- installation/download-mastofe-build.sh
+++ installation/download-mastofe-build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Pleroma: A lightweight social networking server
-# Copyright © 2017-2019 Pleroma Authors <https://pleroma.social/>
+# Copyright © 2017-2020 Pleroma Authors <https://pleroma.social/>
 # SPDX-License-Identifier: AGPL-3.0-only
 project_id="74"
 project_branch="rebase/glitch-soc"
