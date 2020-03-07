14678a7708fb43e60f2f3b610f15d5090616d85c
Alexander Strizhakov <alex.strizhakov@gmail.com>
2020-03-07 10:12:34 +0300

    using `stub` instead `expect`

--- test/http/adapter_helper/gun_test.exs
+++ test/http/adapter_helper/gun_test.exs
@@ -23,7 +23,7 @@ defmodule Pleroma.HTTP.AdapterHelper.GunTest do
 
   defp gun_mock do
     Pleroma.GunMock
-    |> expect(:open, fn _, _, _ -> Task.start_link(fn -> Process.sleep(1000) end) end)
+    |> stub(:open, fn _, _, _ -> Task.start_link(fn -> Process.sleep(1000) end) end)
     |> expect(:await_up, fn _, _ -> {:ok, :http} end)
     |> expect(:set_owner, fn _, _ -> :ok end)
   end
