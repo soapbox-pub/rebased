ad92cef844d4f4211a65fd37b08f8bd8abea8dda
Maksim Pechnikov <parallel588@gmail.com>
2020-04-10 21:27:50 +0300

    fix Oban migration

--- priv/repo/migrations/20200402063221_update_oban_jobs_table.exs
+++ priv/repo/migrations/20200402063221_update_oban_jobs_table.exs
@@ -2,10 +2,10 @@ defmodule Pleroma.Repo.Migrations.UpdateObanJobsTable do
   use Ecto.Migration
 
   def up do
-    Oban.Migrations.up()
+    Oban.Migrations.up(version: 8)
   end
 
   def down do
-    Oban.Migrations.down(version: 1)
+    Oban.Migrations.down(version: 7)
   end
 end
