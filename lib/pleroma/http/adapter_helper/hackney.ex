0cb7b0ea8477bdd7af2e5e9071843be5b8623dff
rinpatch <rinpatch@sdf.org>
2020-05-30 13:59:04 +0300

    hackney adapter helper: support tlsv1.3 and remove custom opts
    - partitial_chain is no longer exported, but it seems to be the default anyway.
    - The bug that caused sni to not be sent automatically seems to be fixed -
    https://github.com/benoitc/hackney/issues/612

--- lib/pleroma/http/adapter_helper/hackney.ex
+++ lib/pleroma/http/adapter_helper/hackney.ex
@@ -22,22 +22,7 @@ defmodule Pleroma.HTTP.AdapterHelper.Hackney do
     |> Pleroma.HTTP.AdapterHelper.maybe_add_proxy(proxy)
   end
 
-  defp add_scheme_opts(opts, %URI{scheme: "http"}), do: opts
-
-  defp add_scheme_opts(opts, %URI{scheme: "https", host: host}) do
-    ssl_opts = [
-      ssl_options: [
-        # Workaround for remote server certificate chain issues
-        partial_chain: &:hackney_connect.partial_chain/1,
-
-        # We don't support TLS v1.3 yet
-        versions: [:tlsv1, :"tlsv1.1", :"tlsv1.2"],
-        server_name_indication: to_charlist(host)
-      ]
-    ]
-
-    Keyword.merge(opts, ssl_opts)
-  end
+  defp add_scheme_opts(opts, _), do: opts
 
   def after_request(_), do: :ok
 end
