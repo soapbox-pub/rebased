8edead7c1dc33457dc30b301b544d71482ef0f28
lain <lain@soykaf.club>
2020-06-03 13:19:38 +0200

    Migration: Remove superfluous imports

--- priv/repo/migrations/20200603105113_add_unique_index_to_chat_message_references.exs
+++ priv/repo/migrations/20200603105113_add_unique_index_to_chat_message_references.exs
@@ -1,13 +1,6 @@
 defmodule Pleroma.Repo.Migrations.BackfillChatMessageReferences do
   use Ecto.Migration
 
-  alias Pleroma.Chat
-  alias Pleroma.ChatMessageReference
-  alias Pleroma.Object
-  alias Pleroma.Repo
-
-  import Ecto.Query
-
   def change do
     create(unique_index(:chat_message_references, [:object_id, :chat_id]))
   end
