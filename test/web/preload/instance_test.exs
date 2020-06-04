a42d135cce3e6326cd8a16f08f4ab83633386c2e
stwf <steven.fuchs@dockyard.com>
2020-06-04 10:51:24 -0400

    test fix

--- test/web/preload/instance_test.exs
+++ test/web/preload/instance_test.exs
@@ -15,7 +15,7 @@ defmodule Pleroma.Web.Preload.Providers.InstanceTest do
              registrations: true
            } = info
 
-    assert String.equivalent?(description, "A Pleroma instance, an alternative fediverse server")
+    assert String.equivalent?(description, "Pleroma: An efficient and flexible fediverse server")
   end
 
   test "it renders the panel", %{"/instance/panel.html": panel} do
