f10b40828fd5f1e4fc7c4ffa9deb543d6a18eb17
rinpatch <rinpatch@sdf.org>
2020-06-07 23:51:03 +0300

    [stable rewrite]: fix streamer streaming out announces from a muted thread
    Use parent object instead of activity because context is null of Announce activity from Mastodon.

--- lib/pleroma/web/streamer/worker.ex
+++ lib/pleroma/web/streamer/worker.ex
@@ -147,7 +147,7 @@ defmodule Pleroma.Web.Streamer.Worker do
          false <- Pleroma.Web.ActivityPub.MRF.subdomain_match?(domain_blocks, item_host),
          false <- Pleroma.Web.ActivityPub.MRF.subdomain_match?(domain_blocks, parent_host),
          true <- thread_containment(item, user),
-         false <- CommonAPI.thread_muted?(user, item) do
+         false <- CommonAPI.thread_muted?(user, parent) do
       true
     else
       _ -> false
