65f3eb333b001586771247ea9949e40bfec0a947
lain <lain@soykaf.club>
2020-06-24 08:50:33 +0000

    Apply suggestion to test/http/tzdata_test.exs

--- test/http/tzdata_test.exs
+++ test/http/tzdata_test.exs
@@ -2,7 +2,7 @@
 # Copyright © 2017-2020 Pleroma Authors <https://pleroma.social/>
 # SPDX-License-Identifier: AGPL-3.0-only
 
-defmodule Pleroma.HTTP.TzdaraTest do
+defmodule Pleroma.HTTP.TzdataTest do
   use ExUnit.Case
 
   import Tesla.Mock
