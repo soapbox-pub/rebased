4e3b3998ad849943fbae3590ce7e892d2dfc54d3
lain <lain@soykaf.club>
2020-07-06 15:44:35 +0200

    BlockValidation test: Rename.

--- test/web/activity_pub/object_validators/block_validation_test.exs
+++ test/web/activity_pub/object_validators/block_validation_test.exs
@@ -2,7 +2,7 @@
 # Copyright © 2017-2020 Pleroma Authors <https://pleroma.social/>
 # SPDX-License-Identifier: AGPL-3.0-only
 
-defmodule Pleroma.Web.ActivityPub.ObjectValidators.BlockHandlingTest do
+defmodule Pleroma.Web.ActivityPub.ObjectValidators.BlockValidationTest do
   use Pleroma.DataCase
 
   alias Pleroma.Web.ActivityPub.Builder
