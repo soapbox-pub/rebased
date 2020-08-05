2a4bca5bd7e33388193d252f9f956d10ce38ad77
Mark Felder <feld@FreeBSD.org>
2020-08-05 11:40:09 -0500

    Comments are good when they're precise...

--- lib/pleroma/user.ex
+++ lib/pleroma/user.ex
@@ -638,7 +638,7 @@ defmodule Pleroma.User do
   @spec force_password_reset(User.t()) :: {:ok, User.t()} | {:error, Ecto.Changeset.t()}
   def force_password_reset(user), do: update_password_reset_pending(user, true)
 
-  # Used to auto-register LDAP accounts which don't have a password hash
+  # Used to auto-register LDAP accounts which won't have a password hash stored locally
   def register_changeset(struct, params = %{password: password})
       when is_nil(password) do
     params = Map.put_new(params, :accepts_chat_messages, true)
