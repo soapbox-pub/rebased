f13f5d9303d45093953a7c609a7b1f282a31e8da
rinpatch <rinpatch@sdf.org>
2021-02-09 22:10:09 +0300

    OpenAPI spec task: Load pleroma application to get version info
    For whatever reason it seems to pick up the version without loading the
    app on my machine, but not on the CI.

--- lib/mix/tasks/pleroma/openapi_spec.ex
+++ lib/mix/tasks/pleroma/openapi_spec.ex
@@ -1,5 +1,7 @@
 defmodule Mix.Tasks.Pleroma.OpenapiSpec do
   def run([path]) do
+    # Load Pleroma application to get version info
+    Application.load(:pleroma)
     spec = Pleroma.Web.ApiSpec.spec(server_specific: false) |> Jason.encode!()
     File.write(path, spec)
   end
