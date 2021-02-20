4796df0bc39a57b2581168cb8d8fde7779068f2d
Mark Felder <feld@feld.me>
2021-02-19 18:36:35 -0600

    Remove Task.async as it is broken here and probably a premature optimization anyway

--- lib/pleroma/web/activity_pub/mrf/followbot_policy.ex
+++ lib/pleroma/web/activity_pub/mrf/followbot_policy.ex
@@ -1,6 +1,5 @@
 defmodule Pleroma.Web.ActivityPub.MRF.FollowbotPolicy do
   @behaviour Pleroma.Web.ActivityPub.MRF
-  alias Pleroma.Activity
   alias Pleroma.Config
   alias Pleroma.User
   alias Pleroma.Web.CommonAPI
@@ -29,31 +28,25 @@ defmodule Pleroma.Web.ActivityPub.MRF.FollowbotPolicy do
   end
 
   defp try_follow(follower, message) do
-    Task.start(fn ->
-      to = Map.get(message, "to", [])
-      cc = Map.get(message, "cc", [])
-      actor = [message["actor"]]
+    to = Map.get(message, "to", [])
+    cc = Map.get(message, "cc", [])
+    actor = [message["actor"]]
 
-      Enum.concat([to, cc, actor])
-      |> List.flatten()
-      |> Enum.uniq()
-      |> User.get_all_by_ap_id()
-      |> Enum.each(fn user ->
-        since_thirty_days_ago = NaiveDateTime.utc_now() |> NaiveDateTime.add(-(86_400 * 30))
+    Enum.concat([to, cc, actor])
+    |> List.flatten()
+    |> Enum.uniq()
+    |> User.get_all_by_ap_id()
+    |> Enum.each(fn user ->
+      with false <- user.local,
+           false <- User.following?(follower, user),
+           false <- User.locked?(user),
+           false <- (user.bio || "") |> String.downcase() |> String.contains?("nobot") do
+        Logger.debug(
+          "#{__MODULE__}: Follow request from #{follower.nickname} to #{user.nickname}"
+        )
 
-        with false <- user.local,
-             false <- User.following?(follower, user),
-             false <- User.locked?(user),
-             false <- (user.bio || "") |> String.downcase() |> String.contains?("nobot"),
-             false <-
-               Activity.follow_requests_outstanding_since?(follower, user, since_thirty_days_ago) do
-          Logger.info(
-            "#{__MODULE__}: Follow request from #{follower.nickname} to #{user.nickname}"
-          )
-
-          CommonAPI.follow(follower, user)
-        end
-      end)
+        CommonAPI.follow(follower, user)
+      end
     end)
 
     {:ok, message}
