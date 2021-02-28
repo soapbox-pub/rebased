0faf8dbef8f0d77fdd42b36ade4d55c42f0ccc8c
Mark Felder <feld@feld.me>
2021-02-28 09:04:29 -0600

    Simplify migration

--- priv/repo/migrations/20210218223811_add_disclose_client_to_users.exs
+++ priv/repo/migrations/20210218223811_add_disclose_client_to_users.exs
@@ -1,15 +1,9 @@
 defmodule Pleroma.Repo.Migrations.AddDiscloseClientToUsers do
   use Ecto.Migration
 
-  def up do
+  def change do
     alter table(:users) do
       add(:disclose_client, :boolean, default: true)
-    end
-  end
-
-  def down do
-    alter table(:users) do
-      remove(:disclose_client)
     end
   end
 end
