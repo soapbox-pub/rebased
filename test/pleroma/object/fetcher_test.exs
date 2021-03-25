6d6bef64bf3b37457b71cf7025e84aa9017a3b86
Haelwenn (lanodan) Monnier <contact@hacktivis.me>
2021-03-25 10:17:26 +0100

    fetcher_test: Remove assert on fake Create having an ap_id

--- test/pleroma/object/fetcher_test.exs
+++ test/pleroma/object/fetcher_test.exs
@@ -132,8 +132,7 @@ defmodule Pleroma.Object.FetcherTest do
       {:ok, object} =
         Fetcher.fetch_object_from_id("http://mastodon.example.org/@admin/99541947525187367")
 
-      assert activity = Activity.get_create_by_object_ap_id(object.data["id"])
-      assert activity.data["id"]
+      assert _activity = Activity.get_create_by_object_ap_id(object.data["id"])
 
       {:ok, object_again} =
         Fetcher.fetch_object_from_id("http://mastodon.example.org/@admin/99541947525187367")
