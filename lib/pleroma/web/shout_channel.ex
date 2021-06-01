48a0ea2fc342c9a757222b0755a0cad9b725bdc7
Mark Felder <feld@feld.me>
2021-06-01 11:55:51 -0500

    Wire up join requests to the old "chat:public" channel into the new "shout:public" channel

--- lib/pleroma/web/shout_channel.ex
+++ lib/pleroma/web/shout_channel.ex
@@ -9,6 +9,9 @@ defmodule Pleroma.Web.ShoutChannel do
   alias Pleroma.Web.MastodonAPI.AccountView
   alias Pleroma.Web.ShoutChannel.ShoutChannelState
 
+  # Backwards compatibility
+  def join("chat:public", message, socket), do: join("shout:public", message, socket)
+
   def join("shout:public", _message, socket) do
     send(self(), :after_join)
     {:ok, socket}
