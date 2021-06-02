297feb73f4ea2de59cf3fc8f4beabe18e72a6311
Mark Felder <feld@feld.me>
2021-06-02 11:21:04 -0500

    Formatting

--- config/description.exs
+++ config/description.exs
@@ -682,7 +682,8 @@ config :pleroma, :config_description, [
       %{
         key: :allow_relay,
         type: :boolean,
-        description: "Enable Pleroma's Relay, which makes it possible to follow a whole instance. (Important!) This will increase the visibility of your instance."
+        description:
+          "Enable Pleroma's Relay, which makes it possible to follow a whole instance. (Important!) This will increase the visibility of your instance."
       },
       %{
         key: :public,
