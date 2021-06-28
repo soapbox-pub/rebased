6815f17435fedf5efad673cbec6a51e8ae0bb963
Alex Gleason <alex@alexgleason.me>
2021-06-28 12:28:27 -0500

    GroupView: return rudimentary Group JSON response

--- lib/pleroma/web/pleroma_api/views/group_view.ex
+++ lib/pleroma/web/pleroma_api/views/group_view.ex
@@ -4,7 +4,34 @@
 defmodule Pleroma.Web.PleromaAPI.GroupView do
   use Pleroma.Web, :view
 
-  def render("show.json", _) do
-    %{}
+  alias Pleroma.Group
+  alias Pleroma.Repo
+  alias Pleroma.User
+
+  def render("show.json", %{group: %Group{} = group}) do
+    group = Repo.preload(group, :user)
+
+    %{
+      id: group.id,
+      # TODO: handle remote accts
+      acct: group.user.nickname,
+      slug: group.user.nickname,
+      avatar: User.avatar_url(group.user),
+      header: User.banner_url(group.user),
+      created_at: group.inserted_at,
+      display_name: group.name,
+      emojis: [],
+      fields: [],
+      # TODO: get proper count
+      members_count: Group.members(group) |> Enum.count(),
+      locked: group.user.is_locked,
+      note: group.description,
+      url: group.ap_id,
+      source: %{
+        fields: [],
+        note: group.description,
+        privacy: group.privacy
+      }
+    }
   end
 end
