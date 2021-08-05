23dfbf1c7e38ed832f0baaeb1a1d338bccfcefe3
Alex Gleason <alex@alexgleason.me>
2021-08-05 19:16:11 +0000

    Revert "Merge branch 'non-rfc-status-codes' into 'develop'"
    This reverts merge request !47

--- config/soapbox.exs
+++ config/soapbox.exs
@@ -16,17 +16,3 @@ config :pleroma, Pleroma.Upload,
     Pleroma.Upload.Filter.Dedupe,
     Pleroma.Upload.Filter.Exiftool
   ]
-
-# Non-RFC HTTP status codes
-config :plug, :statuses, %{
-  # Cloudflare
-  # https://en.wikipedia.org/wiki/List_of_HTTP_status_codes#Cloudflare
-  520 => "Web Server Returned an Unknown Error",
-  521 => "Web Server Is Down",
-  522 => "Connection Timed Out",
-  523 => "Origin Is Unreachable",
-  524 => "A Timeout Occurred",
-  525 => "SSL Handshake Failed",
-  526 => "Invalid SSL Certificate",
-  527 => "Railgun Error"
-}
