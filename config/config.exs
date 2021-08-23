a5bb7f9345ff73469c0d776bce5455ec4f27b4ee
Ekaterina Vaartis <vaartis@kotobank.ch>
2021-08-23 23:52:37 +0300

    Add private_key: nil to default meilisearch options

--- config/config.exs
+++ config/config.exs
@@ -872,7 +872,7 @@ config :pleroma, ConcurrentLimiter, [
 
 config :pleroma, Pleroma.Search, module: Pleroma.Activity.Search
 
-config :pleroma, Pleroma.Search.Meilisearch, url: "http://127.0.0.1:7700/"
+config :pleroma, Pleroma.Search.Meilisearch, url: "http://127.0.0.1:7700/", private_key: nil
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
