0b4fd0d342e3ced073e82355b380cbfee5478c60
Ekaterina Vaartis <vaartis@kotobank.ch>
2021-10-29 13:58:24 +0300

    Set content-type to application/json

--- lib/pleroma/search/meilisearch.ex
+++ lib/pleroma/search/meilisearch.ex
@@ -10,7 +10,8 @@ defmodule Pleroma.Search.Meilisearch do
   defp meili_headers() do
     private_key = Pleroma.Config.get([Pleroma.Search.Meilisearch, :private_key])
 
-    if is_nil(private_key), do: [], else: [{"X-Meili-API-Key", private_key}]
+    [{"Content-Type", "application/json"}] ++
+      if is_nil(private_key), do: [], else: [{"X-Meili-API-Key", private_key}]
   end
 
   def meili_get!(path) do
