da06e1a17fe45407cd82f83223dc68b8920e1fe8
Alex Gleason <alex@alexgleason.me>
2021-11-26 15:32:01 -0600

    v2 Suggestions: add index on is_suggested column

--- priv/repo/migrations/20211126191138_add_suggestions.exs
+++ priv/repo/migrations/20211126191138_add_suggestions.exs
@@ -5,5 +5,7 @@ defmodule Pleroma.Repo.Migrations.AddSuggestions do
     alter table(:users) do
       add(:is_suggested, :boolean, default: false, null: false)
     end
+
+    create_if_not_exists(index(:users, [:is_suggested]))
   end
 end
