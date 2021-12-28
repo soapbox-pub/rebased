fa35e24a5ec70ecd92e9e31d1e13da44b9e27b6d
Alex Gleason <alex@alexgleason.me>
2021-12-27 18:05:35 -0600

    Apps: add user_id index

--- priv/repo/migrations/20210818023112_add_user_id_to_apps.exs
+++ priv/repo/migrations/20210818023112_add_user_id_to_apps.exs
@@ -5,5 +5,7 @@ defmodule Pleroma.Repo.Migrations.AddUserIdToApps do
     alter table(:apps) do
       add(:user_id, references(:users, type: :uuid, on_delete: :delete_all))
     end
+
+    create_if_not_exists(index(:apps, [:user_id]))
   end
 end
