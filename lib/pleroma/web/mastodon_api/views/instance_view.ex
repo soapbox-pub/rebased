ed6973d3d9b6bba46f5112b49620a49b62c58d76
NEETzsche <neetzsche@tutanota.com>
2022-01-14 20:52:31 +0000

    Show blockers_visible in /api/v1/instance

--- lib/pleroma/web/mastodon_api/views/instance_view.ex
+++ lib/pleroma/web/mastodon_api/views/instance_view.ex
@@ -39,6 +39,7 @@ defmodule Pleroma.Web.MastodonAPI.InstanceView do
       background_image: Pleroma.Web.Endpoint.url() <> Keyword.get(instance, :background_image),
       shout_limit: Config.get([:shout, :limit]),
       description_limit: Keyword.get(instance, :description_limit),
+      blockers_visible: Config.get([:activitypub, :blockers_visible]),
       pleroma: %{
         metadata: %{
           account_activation_required: Keyword.get(instance, :account_activation_required),
