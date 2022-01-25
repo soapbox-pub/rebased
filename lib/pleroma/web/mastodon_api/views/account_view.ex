c1ae35ff2c4620cb6958d0c6910c79ed17f62db9
marcin mikołajczak <git@mkljczk.pl>
2022-01-25 17:04:41 +0100

    Fix show_birthday
    Signed-off-by: marcin mikołajczak <git@mkljczk.pl>

--- lib/pleroma/web/mastodon_api/views/account_view.ex
+++ lib/pleroma/web/mastodon_api/views/account_view.ex
@@ -298,7 +298,6 @@ defmodule Pleroma.Web.MastodonAPI.AccountView do
         background_image: image_url(user.background) |> MediaProxy.url(),
         accepts_chat_messages: user.accepts_chat_messages,
         favicon: favicon,
-        birthday: user.birthday
       }
     }
     |> maybe_put_role(user, opts[:for])
