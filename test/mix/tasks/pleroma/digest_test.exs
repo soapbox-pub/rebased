8b0c2890f99feae374628d96ae1e65949e7631f5
Tusooa Zhu <tusooa@kazv.moe>
2022-03-01 20:27:45 -0500

    Fix digest test

--- test/mix/tasks/pleroma/digest_test.exs
+++ test/mix/tasks/pleroma/digest_test.exs
@@ -53,7 +53,13 @@ defmodule Mix.Tasks.Pleroma.DigestTest do
 
       assert_email_sent(
         to: {user2.name, user2.email},
-        html_body: ~r/here is what you've missed!/i
+        html_body:
+          Regex.compile!(
+            "here is what you've missed!"
+            |> Phoenix.HTML.html_escape()
+            |> Phoenix.HTML.safe_to_string(),
+            "i"
+          )
       )
     end
   end
