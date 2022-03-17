3e6cf43ec828f1b3fdca99e58d25da1be71f565b
Alex Gleason <alex@alexgleason.me>
2022-03-17 12:40:09 -0500

    Add landing-fe, remove Soapbox FE build

--- instance/static/frontends/soapbox-fe/vendor/packs/js/emoji_picker-7e7b4d7ca5d73328f3ef.chunk.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{652:function(n,r,i){"use strict";i.r(r);var o=i(585);i.d(r,"Picker",function(){return o.a});var c=i(234);i.d(r,"Emoji",function(){return c.a})}}]);
-//# sourceMappingURL=emoji_picker-7e7b4d7ca5d73328f3ef.chunk.js.map
