7bc95acca091928b7e43545a3deae1b75d379ded
marcin mikołajczak <git@mkljczk.pl>
2022-03-19 18:28:42 +0100

    Add "status" notification type
    Signed-off-by: marcin mikołajczak <git@mkljczk.pl>

--- test/pleroma/notification_test.exs
+++ test/pleroma/notification_test.exs
@@ -104,6 +104,7 @@ defmodule Pleroma.NotificationTest do
       {:ok, [notification]} = Notification.create_notifications(status)
 
       assert notification.user_id == subscriber.id
+      assert notification.type == "status"
     end
 
     test "does not create a notification for subscribed users if status is a reply" do
