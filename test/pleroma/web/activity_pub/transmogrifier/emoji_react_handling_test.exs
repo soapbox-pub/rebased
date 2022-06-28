11f9f2ef277937d5558a1cc0a92a60b872f17de0
Hélène <pleroma-dev@helene.moe>
2022-06-28 20:33:50 +0200

    EmojiReactValidator: fix emoji qualification
    Tries fully-qualifying emoji when receiving them, by adding the emoji
    variation sequence to the received reaction emoji.
    
    This issue arises when other instance software, such as Misskey, tries
    reacting with emoji that have unqualified or minimally qualified
    variants, like a red heart. Pleroma only accepts fully qualified emoji
    in emoji reactions, and refused those emoji. Now, Pleroma will attempt
    to properly qualify them first, and reject them if checks still fail.

--- test/pleroma/web/activity_pub/transmogrifier/emoji_react_handling_test.exs
+++ test/pleroma/web/activity_pub/transmogrifier/emoji_react_handling_test.exs
@@ -37,6 +37,32 @@ defmodule Pleroma.Web.ActivityPub.Transmogrifier.EmojiReactHandlingTest do
     assert match?([["👌", _]], object.data["reactions"])
   end
 
+  test "it works for incoming unqualified emoji reactions" do
+    user = insert(:user)
+    other_user = insert(:user, local: false)
+    {:ok, activity} = CommonAPI.post(user, %{status: "hello"})
+
+    data =
+      File.read!("test/fixtures/emoji-reaction-unqualified.json")
+      |> Jason.decode!()
+      |> Map.put("object", activity.data["object"])
+      |> Map.put("actor", other_user.ap_id)
+
+    {:ok, %Activity{data: data, local: false}} = Transmogrifier.handle_incoming(data)
+
+    assert data["actor"] == other_user.ap_id
+    assert data["type"] == "EmojiReact"
+    assert data["id"] == "http://mastodon.example.org/users/admin#reactions/2"
+    assert data["object"] == activity.data["object"]
+    # heart emoji with added emoji variation sequence
+    assert data["content"] == "❤️"
+
+    object = Object.get_by_ap_id(data["object"])
+
+    assert object.data["reaction_count"] == 1
+    assert match?([["❤️", _]], object.data["reactions"])
+  end
+
   test "it reject invalid emoji reactions" do
     user = insert(:user)
     other_user = insert(:user, local: false)
