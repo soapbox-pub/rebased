3885ee182a572a10b326ae553703ee0d38f3b66d
Tusooa Zhu <tusooa@kazv.moe>
2022-07-11 15:49:58 -0400

    Switch to associated_object_id index

--- lib/pleroma/web/activity_pub/activity_pub.ex
+++ lib/pleroma/web/activity_pub/activity_pub.ex
@@ -1150,8 +1150,7 @@ defmodule Pleroma.Web.ActivityPub.ActivityPub do
       [activity, object: o] in query,
       where:
         fragment(
-          "(?)->>'type' = 'Create' and coalesce((?)->'object'->>'id', (?)->>'object') = any (?)",
-          activity.data,
+          "(?)->>'type' = 'Create' and associated_object_id((?)) = any (?)",
           activity.data,
           activity.data,
           ^ids
