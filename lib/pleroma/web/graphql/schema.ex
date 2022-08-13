b200820e7b75af21d9fdf7f75861040020d1e34c
Alex Gleason <alex@alexgleason.me>
2022-08-13 15:19:03 -0500

    graphql "hello world"

---
+++ lib/pleroma/web/graphql/schema.ex
@@ -0,0 +1,14 @@
+defmodule Pleroma.Web.GraphQL.Schema do
+  use Absinthe.Schema
+
+
+  object :hello do
+    field :text, :string
+  end
+
+  query do
+    field :hello, :hello do
+      resolve(fn _, _, _ -> {:ok, %{text: "world"}} end)
+    end
+  end
+end
