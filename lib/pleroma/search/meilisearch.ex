6256822afd368e5f6b410d47c5ff9b584e50a461
Ekaterina Vaartis <vaartis@kotobank.ch>
2022-08-27 01:11:50 +0300

    Check for updateId, not uid

--- lib/pleroma/search/meilisearch.ex
+++ lib/pleroma/search/meilisearch.ex
@@ -153,7 +153,7 @@ defmodule Pleroma.Search.Meilisearch do
         )
 
       with {:ok, res} <- result,
-           true <- Map.has_key?(res, "uid") do
+           true <- Map.has_key?(res, "updateId") do
         # Added successfully
         :ok
       else
