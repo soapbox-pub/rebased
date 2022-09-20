e66c02b77516758dda14a9b015f1e5d0b28b93b9
Tusooa Zhu <tusooa@kazv.moe>
2022-09-20 12:34:10 -0400

    Make instance document controller test sync

--- test/pleroma/web/admin_api/controllers/instance_document_controller_test.exs
+++ test/pleroma/web/admin_api/controllers/instance_document_controller_test.exs
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 
 defmodule Pleroma.Web.AdminAPI.InstanceDocumentControllerTest do
-  use Pleroma.Web.ConnCase, async: true
+  use Pleroma.Web.ConnCase
   import Pleroma.Factory
 
   @dir "test/tmp/instance_static"
