272e022a71031d4b12f345b3749af9e0493df3b3
Alex Gleason <alex@alexgleason.me>
2022-11-06 13:27:45 -0600

    OpenGraphTest: fix MediaProxy URL

--- test/pleroma/web/metadata/providers/open_graph_test.exs
+++ test/pleroma/web/metadata/providers/open_graph_test.exs
@@ -144,7 +144,7 @@ defmodule Pleroma.Web.Metadata.Providers.OpenGraphTest do
             [
               property: "og:image",
               content:
-                "http://localhost:4001/proxy/preview/LzAnlke-l5oZbNzWsrHfprX1rGw/aHR0cHM6Ly9wbGVyb21hLmdvdi9hYm91dC9qdWNoZS53ZWJt/juche.webm"
+                "http://localhost:4001/proxy/preview/YyXfYXyaEAGVHVfKF-HRc4GrFMY03Smi7effZ8WKow8/aHR0cHM6Ly9wbGVyb21hLmdvdi9hYm91dC9qdWNoZS53ZWJt/juche.webm"
             ], []} in result
   end
 
