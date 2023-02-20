75b76a0666b0de025dec2539e8be4b6d18e8de69
tusooa <tusooa@kazv.moe>
2023-02-20 12:32:45 -0500

    Bump version in mix project to 2.5.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Pleroma.Mixfile do
   def project do
     [
       app: :pleroma,
-      version: version("2.5.0"),
+      version: version("2.5.1"),
       elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
