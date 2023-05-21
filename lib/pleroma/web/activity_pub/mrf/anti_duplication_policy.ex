7adead8df8e8d58cb785f7cd57def41d56bc2b4b
Alex Gleason <alex@alexgleason.me>
2023-05-21 16:12:33 -0500

    AntiDuplicationPolicy: fix messing up Announces

--- lib/pleroma/web/activity_pub/mrf/anti_duplication_policy.ex
+++ lib/pleroma/web/activity_pub/mrf/anti_duplication_policy.ex
@@ -5,11 +5,9 @@ defmodule Pleroma.Web.ActivityPub.MRF.AntiDuplicationPolicy do
   @cachex Pleroma.Config.get([:cachex, :provider], Cachex)
   @cache :anti_duplication_mrf_cache
 
-  @object_types ~w[Note Article Page ChatMessage Question Event]
-
   @impl true
-  def filter(%{"type" => type, "content" => content} = object)
-      when is_binary(content) and type in @object_types do
+  def filter(%{"type" => "Create", "object" => %{"content" => content} = object})
+      when is_binary(content) do
     ttl = Pleroma.Config.get([:mrf_anti_duplication, :ttl], :timer.minutes(1))
     min_length = Pleroma.Config.get([:mrf_anti_duplication, :min_length], 50)
 
@@ -29,11 +27,6 @@ defmodule Pleroma.Web.ActivityPub.MRF.AntiDuplicationPolicy do
     else
       {:ok, object}
     end
-  end
-
-  def filter(%{"object" => %{"type" => type, "content" => content} = object})
-      when is_binary(content) and type in @object_types do
-    filter(object)
   end
 
   def filter(object) do
