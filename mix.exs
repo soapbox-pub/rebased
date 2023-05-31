bed44f9f628c3e184ba367765c87c47656060f28
Mark Felder <feld@feld.me>
2023-05-31 13:10:18 -0400

    Update to Phoenix 1.7
    Chase the other deps to match versions you get from a current "mix phx.new" project creation

--- mix.exs
+++ mix.exs
@@ -113,18 +113,19 @@ defmodule Pleroma.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.6.0"},
-      {:phoenix_ecto, "~> 4.4.0"},
+      {:phoenix, "~> 1.7.3"},
+      {:phoenix_ecto, "~> 4.4"},
+      {:ecto_sql, "~> 3.10"},
       {:ecto_enum, "~> 1.4"},
       {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 3.1"},
+      {:phoenix_html, "~> 3.3"},
       {:phoenix_live_reload, "~> 1.3.3", only: :dev},
-      {:phoenix_live_view, "~> 0.17.1"},
-      {:phoenix_live_dashboard, "~> 0.6.2"},
-      {:telemetry_metrics, "~> 0.6.1"},
+      {:phoenix_live_view, "~> 0.19.0"},
+      {:phoenix_live_dashboard, "~> 0.8.0"},
+      {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:tzdata, "~> 1.0.3"},
-      {:plug_cowboy, "~> 2.3"},
+      {:plug_cowboy, "~> 2.5"},
       # oban 2.14 requires Elixir 1.12+
       {:oban, "~> 2.13.4"},
       {:gettext,
