ac55764599c054a03299470d92fd0d274509a024
Mark Felder <feld@feld.me>
2024-02-22 14:07:46 -0500

    Gun Connection Pool was not attempting to free a connection and retry once if the pool was full.

--- lib/pleroma/gun/connection_pool/worker_supervisor.ex
+++ lib/pleroma/gun/connection_pool/worker_supervisor.ex
@@ -21,7 +21,7 @@ defmodule Pleroma.Gun.ConnectionPool.WorkerSupervisor do
   def start_worker(opts, retry \\ false) do
     case DynamicSupervisor.start_child(__MODULE__, {Pleroma.Gun.ConnectionPool.Worker, opts}) do
       {:error, :max_children} ->
-        funs = [fn -> !retry end, fn -> match?(:error, free_pool()) end]
+        funs = [fn -> retry end, fn -> match?(:error, free_pool()) end]
 
         if Enum.any?(funs, fn fun -> fun.() end) do
           :telemetry.execute([:pleroma, :connection_pool, :provision_failure], %{opts: opts})
