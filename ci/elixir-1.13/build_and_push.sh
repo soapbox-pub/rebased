f8411a351de07f14fdc9c9eca30109feaadf6f93
Lain Soykaf <lain@lain.com>
2024-05-20 13:30:31 +0400

    CI: Specify version fully in base image tag

--- ci/elixir-1.13/build_and_push.sh
+++ ci/elixir-1.13/build_and_push.sh
@@ -1,1 +1,1 @@
-docker buildx build --platform linux/amd64,linux/arm64 -t git.pleroma.social:5050/pleroma/pleroma/ci-base:elixir-1.13-otp24 --push .
+docker buildx build --platform linux/amd64,linux/arm64 -t git.pleroma.social:5050/pleroma/pleroma/ci-base:elixir-1.13.4-otp24 --push .
