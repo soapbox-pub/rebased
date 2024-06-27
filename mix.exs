436286c93f37c1494246834e6bd656ef0b089b7c
Mark Felder <feld@feld.me>
2024-06-27 11:52:54 -0400

    Update Tesla to 1.11.0

--- mix.exs
+++ mix.exs
@@ -152,7 +152,7 @@ defmodule Pleroma.Mixfile do
       {:calendar, "~> 1.0"},
       {:cachex, "~> 3.2"},
       {:poison, "~> 3.0", override: true},
-      {:tesla, "~> 1.8.0"},
+      {:tesla, "~> 1.11"},
       {:castore, "~> 0.1"},
       {:cowlib, "~> 2.9", override: true},
       {:gun, "~> 2.0.0-rc.1", override: true},
