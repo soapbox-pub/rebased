c6775e40c8af1d368102fe89f8797aee6a0f5952
marcin mikołajczak <git@mkljczk.pl>
2024-08-24 01:06:16 +0200

    Update migration
    Signed-off-by: marcin mikołajczak <git@mkljczk.pl>

--- priv/repo/migrations/20220527134341_add_quote_url_index_to_objects.exs
+++ priv/repo/migrations/20220527134341_add_quote_url_index_to_objects.exs
@@ -5,6 +5,7 @@
 defmodule Pleroma.Repo.Migrations.AddQuoteUrlIndexToObjects do
   use Ecto.Migration
   @disable_ddl_transaction true
+  @disable_migration_lock true
 
   def change do
     create_if_not_exists(
