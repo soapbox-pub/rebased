bc16f09d7b9c1a15c8c9be6d99092b9a8d867d18
Mark Felder <feld@feld.me>
2024-09-06 11:12:13 -0400

    Dialyzer: the pattern can never match the type
    The original error was for the chat controller:
    
    lib/pleroma/web/pleroma_api/controllers/chat_controller.ex:104:pattern_match The pattern can never match the type {:error, :content_too_long | :forbidden | :no_content | :not_found} | {:user, nil}.
    
    Improve typespecs for the Pipeline and apply them where it could be encountered

--- lib/pleroma/object/fetcher.ex
+++ lib/pleroma/object/fetcher.ex
@@ -62,7 +62,8 @@ defmodule Pleroma.Object.Fetcher do
            :error | :reject | :allowed_depth | :fetch | :containment | :transmogrifier
 
   # Note: will create a Create activity, which we need internally at the moment.
-  @spec fetch_object_from_id(String.t(), list()) :: {:ok, Object.t()} | {fetcher_errors(), any()}
+  @spec fetch_object_from_id(String.t(), list()) ::
+          {:ok, Object.t()} | {fetcher_errors(), any()} | Pipeline.errors()
   def fetch_object_from_id(id, options \\ []) do
     with {_, nil} <- {:fetch_object, Object.get_cached_by_ap_id(id)},
          {_, true} <- {:allowed_depth, Federator.allowed_thread_distance?(options[:depth])},
