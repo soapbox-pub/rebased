23f548dc614515800589799eeda93aae8d8ae11f
marcin mikołajczak <git@mkljczk.pl>
2024-09-10 22:28:50 +0200

    Akkoma migration: Move mastofe settings to settings store
    Signed-off-by: marcin mikołajczak <git@mkljczk.pl>

--- priv/repo/optional_migrations/akkoma_rollbacks/20220108213213_add_mastofe_settings.exs
+++ priv/repo/optional_migrations/akkoma_rollbacks/20220108213213_add_mastofe_settings.exs
@@ -7,6 +7,16 @@ defmodule Pleroma.Repo.Migrations.AddMastofeSettings do
   def up, do: :ok
 
   def down do
+    """
+    UPDATE users SET pleroma_settings_store = jsonb_set(
+      pleroma_settings_store,
+      '{glitch-lily}',
+      mastofe_settings
+    )
+    WHERE mastofe_settings IS NOT NULL;
+    """
+    |> execute()
+
     alter table(:users) do
       remove_if_exists(:mastofe_settings, :map)
     end
