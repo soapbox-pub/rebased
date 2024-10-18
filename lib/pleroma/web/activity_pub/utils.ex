e79dcd2bfec23c45e88d51653a542a59508b651f
marcin mikołajczak <git@mkljczk.pl>
2024-10-18 15:46:00 +0200

    Fix rejecting duplicated bites
    Signed-off-by: marcin mikołajczak <git@mkljczk.pl>

--- lib/pleroma/web/activity_pub/utils.ex
+++ lib/pleroma/web/activity_pub/utils.ex
@@ -971,6 +971,7 @@ defmodule Pleroma.Web.ActivityPub.Utils do
     |> maybe_exclude_activity_id(exclude_activity)
     |> Activity.Queries.by_object_id(bitten_ap_id)
     |> order_by([activity], fragment("? desc nulls last", activity.id))
+    |> exclude_rejected()
     |> limit(1)
     |> Repo.one()
   end
@@ -980,5 +981,14 @@ defmodule Pleroma.Web.ActivityPub.Utils do
   defp maybe_exclude_activity_id(query, %Activity{id: activity_id}) do
     query
     |> where([a], a.id != ^activity_id)
+  end
+
+  defp exclude_rejected(query) do
+    rejected_activities = "Reject"
+    |> Activity.Queries.by_type()
+    |> select([a], fragment("?->>'object'", a.data))
+
+    query
+    |> where([a], fragment("?->>'id'", a.data) not in subquery(rejected_activities))
   end
 end
