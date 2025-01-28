8cd77168726e2e44d7612c29914c6b6398ff675d
mkljczk <git@mkljczk.pl>
2025-01-28 22:28:34 +0100

    Fix Mastodon incoming edits with inlined "likes"
    Signed-off-by: mkljczk <git@mkljczk.pl>

--- lib/pleroma/web/activity_pub/object_validators/question_validator.ex
+++ lib/pleroma/web/activity_pub/object_validators/question_validator.ex
@@ -64,6 +64,7 @@ defmodule Pleroma.Web.ActivityPub.ObjectValidators.QuestionValidator do
     |> CommonFixes.fix_actor()
     |> CommonFixes.fix_object_defaults()
     |> CommonFixes.fix_quote_url()
+    |> CommonFixes.fix_likes()
     |> Transmogrifier.fix_emoji()
     |> fix_closed()
   end
