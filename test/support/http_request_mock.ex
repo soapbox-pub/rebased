9e366c3de15a830f1ba21c8b144fd3e1fe7bd557
Alex Gleason <alex@alexgleason.me>
2025-03-14 18:28:17 -0500

    Fix duplicate request mocks

--- test/support/http_request_mock.ex
+++ test/support/http_request_mock.ex
@@ -1431,29 +1431,11 @@ defmodule HttpRequestMock do
      }}
   end
 
-  def get("https://friends.grishka.me/posts/54642", _, _, _) do
-    {:ok,
-     %Tesla.Env{
-       status: 200,
-       body: File.read!("test/fixtures/tesla_mock/smithereen_non_anonymous_poll.json"),
-       headers: activitypub_object_headers()
-     }}
-  end
-
   def get("https://mk.absturztau.be/users/8ozbzjs3o8", _, _, _) do
     {:ok,
      %Tesla.Env{
        status: 200,
        body: File.read!("test/fixtures/tesla_mock/mametsuko@mk.absturztau.be.json"),
-       headers: activitypub_object_headers()
-     }}
-  end
-
-  def get("https://friends.grishka.me/users/1", _, _, _) do
-    {:ok,
-     %Tesla.Env{
-       status: 200,
-       body: File.read!("test/fixtures/tesla_mock/smithereen_user.json"),
        headers: activitypub_object_headers()
      }}
   end
@@ -1519,120 +1501,6 @@ defmodule HttpRequestMock do
        status: 200,
        body: File.read!("test/fixtures/tesla_mock/nominatim_single_result.json"),
        headers: [{"content-type", "application/json"}]
-     }}
-  end
-
-  def get("https://mastodon.example/.well-known/host-meta", _, _, _) do
-    {:ok,
-     %Tesla.Env{
-       status: 302,
-       headers: [{"location", "https://sub.mastodon.example/.well-known/host-meta"}]
-     }}
-  end
-
-  def get("https://sub.mastodon.example/.well-known/host-meta", _, _, _) do
-    {:ok,
-     %Tesla.Env{
-       status: 200,
-       body:
-         "test/fixtures/webfinger/masto-host-meta.xml"
-         |> File.read!()
-         |> String.replace("{{domain}}", "sub.mastodon.example")
-     }}
-  end
-
-  def get(
-        "https://sub.mastodon.example/.well-known/webfinger?resource=acct:a@mastodon.example",
-        _,
-        _,
-        _
-      ) do
-    {:ok,
-     %Tesla.Env{
-       status: 200,
-       body:
-         "test/fixtures/webfinger/masto-webfinger.json"
-         |> File.read!()
-         |> String.replace("{{nickname}}", "a")
-         |> String.replace("{{domain}}", "mastodon.example")
-         |> String.replace("{{subdomain}}", "sub.mastodon.example"),
-       headers: [{"content-type", "application/jrd+json"}]
-     }}
-  end
-
-  def get("https://sub.mastodon.example/users/a", _, _, _) do
-    {:ok,
-     %Tesla.Env{
-       status: 200,
-       body:
-         "test/fixtures/webfinger/masto-user.json"
-         |> File.read!()
-         |> String.replace("{{nickname}}", "a")
-         |> String.replace("{{domain}}", "sub.mastodon.example"),
-       headers: [{"content-type", "application/activity+json"}]
-     }}
-  end
-
-  def get("https://sub.mastodon.example/users/a/collections/featured", _, _, _) do
-    {:ok,
-     %Tesla.Env{
-       status: 200,
-       body:
-         File.read!("test/fixtures/users_mock/masto_featured.json")
-         |> String.replace("{{domain}}", "sub.mastodon.example")
-         |> String.replace("{{nickname}}", "a"),
-       headers: [{"content-type", "application/activity+json"}]
-     }}
-  end
-
-  def get("https://pleroma.example/.well-known/host-meta", _, _, _) do
-    {:ok,
-     %Tesla.Env{
-       status: 302,
-       headers: [{"location", "https://sub.pleroma.example/.well-known/host-meta"}]
-     }}
-  end
-
-  def get("https://sub.pleroma.example/.well-known/host-meta", _, _, _) do
-    {:ok,
-     %Tesla.Env{
-       status: 200,
-       body:
-         "test/fixtures/webfinger/pleroma-host-meta.xml"
-         |> File.read!()
-         |> String.replace("{{domain}}", "sub.pleroma.example")
-     }}
-  end
-
-  def get(
-        "https://sub.pleroma.example/.well-known/webfinger?resource=acct:a@pleroma.example",
-        _,
-        _,
-        _
-      ) do
-    {:ok,
-     %Tesla.Env{
-       status: 200,
-       body:
-         "test/fixtures/webfinger/pleroma-webfinger.json"
-         |> File.read!()
-         |> String.replace("{{nickname}}", "a")
-         |> String.replace("{{domain}}", "pleroma.example")
-         |> String.replace("{{subdomain}}", "sub.pleroma.example"),
-       headers: [{"content-type", "application/jrd+json"}]
-     }}
-  end
-
-  def get("https://sub.pleroma.example/users/a", _, _, _) do
-    {:ok,
-     %Tesla.Env{
-       status: 200,
-       body:
-         "test/fixtures/webfinger/pleroma-user.json"
-         |> File.read!()
-         |> String.replace("{{nickname}}", "a")
-         |> String.replace("{{domain}}", "sub.pleroma.example"),
-       headers: [{"content-type", "application/activity+json"}]
      }}
   end
 
